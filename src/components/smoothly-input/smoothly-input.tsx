102dbdf6e9c2994478e56259fba63f011af23711
Simon Mika <simon@mika.se>
2019-02-19 20:33:40 +0100

    Improved css.

--- src/components/smoothly-input/smoothly-input.tsx
+++ src/components/smoothly-input/smoothly-input.tsx
@@ -1,4 +1,4 @@
-import { Component, State, Prop } from "@stencil/core"
+import { Component, Prop } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-input",
@@ -13,35 +13,17 @@ export class SmoothlyInput {
 	@Prop() tabIndex: number
 	@Prop() placeholder?: string
 
-	@State() focused: string = "form-group"
-
-	protected async onInput(e: Event): Promise<boolean> {
+	protected async onInput(e: UIEvent): Promise<boolean> {
+		if (e.target && (e.target as HTMLInputElement).value)
+			this.value = (e.target as HTMLInputElement).value
 		return true
 	}
-
 	// Placeholder animation
-	private focusMe(e: any){
-		this.focused = "form-group focused"
-		e.target.classList.add("active")
-		return e
-	}
-	private blurMe(e: any){
-		const inputValue = e.target.value
-		if (inputValue == "") {
-			e.target.classList.remove("filled")
-			e.target.classList.remove("active")
-			this.focused = "form-group"
-		} else {
-			e.target.classList.add("filled")
-			e.target.classList.remove("active")
-		}
-		return e
-	}
 	render() {
 		return [
-			<div class={ this.focused }>
-				<label class="form-label" htmlFor={this.name}><slot/></label>
-				<input type={this.type} class="form-input" placeholder={this.placeholder} name={this.name} maxlength={this.maxLength} inputmode={this.inputMode} tabindex={this.tabIndex} onFocus={e => { this.focusMe(e) } }  onBlur={ e => { this.blurMe(e) } } value={this.value} onInput={ e => { this.onInput(e) }}/>
+			<div class={ this.value && this.value.length > 0 ? "has-content" : "" }>
+				<label htmlFor={this.name}><slot/></label>
+				<input type={this.type} placeholder={this.placeholder} name={this.name} maxlength={this.maxLength} inputmode={this.inputMode} tabindex={this.tabIndex} value={this.value} onInput={ e => this.onInput(e as UIEvent) }/>
 			</div>,
 		]
 	}
