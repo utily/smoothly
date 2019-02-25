905a82492c9d64f4aeae4290f044a3194ead44f2
Simon Mika <simon@mika.se>
2019-02-25 14:33:30 +0100

    Added tests to input.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,5 @@
 import { Component, Event, EventEmitter, Prop } from "@stencil/core"
-
+import { Autocomplete } from "./Autocomplete"
 @Component({
 	tag: "smoothly-input",
 	styleUrl: "style.css",
@@ -9,17 +9,13 @@ export class SmoothlyInput {
 	@Prop() name: string
 	@Prop({ mutable: true }) value: string
 	@Prop() type: "text" | "email" = "text"
-	@Prop() maxLength?: number
-	@Prop() inputMode: string
-	@Prop() tabIndex: number
 	@Prop() placeholder?: string
-	@Prop({ mutable: true, reflectToAttr: true }) valid: boolean
-	@Prop({ mutable: true, reflectToAttr: true }) mandatory: boolean
-	@Event() changed: EventEmitter<SmoothlyInput>
+	@Prop({ mutable: true, reflectToAttr: true }) required: boolean
+	@Prop({ mutable: true, reflectToAttr: true }) autocomplete: Autocomplete
+	@Prop({ mutable: true, reflectToAttr: true }) pattern?: string
 	protected async onInput(e: UIEvent) {
 		if (e.target && (e.target as HTMLInputElement).value) {
 			this.value = (e.target as HTMLInputElement).value
-			this.changed.emit(this)
 		}
 	}
 	hostData() {
@@ -28,7 +24,15 @@ export class SmoothlyInput {
 	// Placeholder animation
 	render() {
 		return [
-			<input type={this.type} placeholder={this.placeholder} name={this.name} maxlength={this.maxLength} inputmode={this.inputMode} tabindex={this.tabIndex} value={this.value} onInput={ e => this.onInput(e as UIEvent) }></input>,
+			<input
+				name={this.name}
+				value={this.value}
+				type={this.type}
+				placeholder={this.placeholder}
+				required={this.required}
+				autocomplete={this.autocomplete}
+				pattern={this.pattern}
+				onInput={ e => this.onInput(e as UIEvent) }></input>,
 			<label htmlFor={this.name}><slot/></label>,
 		]
 	}
