466356a6c681b3139fb0b7c4ab25c6d7342836db
Johnny Olsson <23193083+johnnysolsson@users.noreply.github.com>
2019-04-11 10:52:22 +0200

    Action (#14)
    * New action component
    
    * Polished action component and added it to example
    
    * Updated style

--- src/components/action/index.tsx
+++ src/components/action/index.tsx
@@ -1,11 +1,16 @@
 import { Component, Prop, Event, EventEmitter, Listen } from "@stencil/core"
-
+import { Color } from "../../Color"
+import { Expand } from "../../Expand"
+import { Fill } from "../../Fill"
 @Component({
 	tag: "smoothly-action",
 	styleUrl: "style.css",
 	scoped: true,
 })
 export class SmoothlyAction {
+	@Prop({ reflectToAttr: true }) color: Color | undefined
+	@Prop({ reflectToAttr: true }) expand: Expand
+	@Prop({ reflectToAttr: true }) fill: Fill
 	@Prop() name: string
 	@Event() smoothlyAction: EventEmitter
 
