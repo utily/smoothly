99cc4eaa6df2b0cfb7bfbb58befc058e70968372
Simon Mika <simonmika@users.noreply.github.com>
2019-05-14 08:59:57 +0200

    Inter-frame messaging works. (#19)

--- src/Message.ts
+++ src/Message.ts
@@ -5,34 +5,39 @@ export interface Message<T> {
 export class Message<T> {
 	static is(value: Message<any> | any): value is Message<any> {
 		return typeof(value) == "object" &&
-			typeof(value.destination) == "string"
+			typeof(value.destination) == "string" &&
+			value.content != undefined
 	}
-	static send(message: Message<any>, context?: Window, origin?: string): void
-	static send(destination: string, content: any, context?: Window, origin?: string): void
-	static send(message: string | Message<any>, content?: any | Window, context?: Window | string, origin?: string): void {
-		if (Message.is(message) && (typeof(context) == "string" || context == undefined) && origin == undefined) {
-			origin = context
+	static send(message: Message<any>, context?: Window): void
+	static send(destination: string, content: any, context?: Window): void
+	static send(message: string | Message<any>, content?: any | Window, context?: Window): void {
+		console.log("message.send", { message, content, context })
+		if (Message.is(message) && context == undefined) {
 			context = content as Window
 			if (!context)
 				context = window
-			if (!origin)
-				origin = context.location.origin
-			content.postMessage(message, context)
+			const destination = message.destination.split("#", 2)
+			message = { destination: destination[1], content: message.content }
+			console.log("postMessage", message, destination[0])
+			context.postMessage(message, destination[0])
 		} else if (typeof(context) != "string") {
 			if (!context)
 				context = window
-			if (!origin)
-				origin = context.location.origin
 			if (typeof(message) == "string")
-			Message.send({ destination: message, content }, context, origin)
+			Message.send({ destination: message, content }, context)
 		}
 	}
-	static listen(handle: (destination: string, content: any) => void, context?: Window, origin?: string): void {
+	static listen(origin: string, handle: (destination: string, content: any) => void, context?: Window): void {
+		const splitted = origin.split("#", 2)
+		let destination = ""
+		if (splitted.length == 2) {
+			origin = splitted[0]
+			destination = splitted[1]
+		}
 		(context || window).addEventListener("message", (e: MessageEvent) => {
+			console.log("message.listen callback", origin, { message: e.data, origin: e.origin })
 			const message = e.data
-			if (!origin)
-				origin = (context || window).location.origin
-			if (Message.is(message) && (origin == "*" || e.origin == origin))
+			if (Message.is(message) && (origin == "*" || e.origin == origin) && (destination == "" || message.destination == destination))
 				handle(message.destination, message.content)
 		})
 	}
