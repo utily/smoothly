8eb5f888a33894a1ea39026f443cdb0bd64238dd
Simon Mika <simon@mika.se>
2019-06-11 09:14:00 +0200

    Changed name of property.

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -8,7 +8,7 @@ import { Component, Event, EventEmitter, Prop, h } from "@stencil/core"
 export class SmoothlyCheckbox {
 	@Prop() name: string
 	@Prop() value: string
-	@Prop() title: string
+	@Prop() label: string
 	@Prop({ mutable: true, reflectToAttr: true }) checked: boolean
 	@Event() smoothlyChecked!: EventEmitter<{ name: string, value: string }>
 
@@ -20,7 +20,7 @@ export class SmoothlyCheckbox {
 
 	render() {
 		return [
-			<input type="checkbox" name={ this.name } value={ this.value } title={ this.title } checked={ this.checked } onChange={ e => this.onInput(e as UIEvent) }/>,
+			<input type="checkbox" name={ this.name } value={ this.value } title={ this.label } checked={ this.checked } onChange={ e => this.onInput(e as UIEvent) }/>,
 		]
 	}
 }
