857a024ea6a0d5b8c75c83562061ba7cf6ae9ae4
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-12 10:24:52 +0200

    Switched html elements and reworked css

--- src/components/notice/index.tsx
+++ src/components/notice/index.tsx
@@ -3,11 +3,11 @@ import { Component, Prop, h, Listen } from "@stencil/core"
 import { Trigger, Notice } from "smoothly-model"
 
 @Component({
-	tag: "smoothly-notice-display",
+	tag: "smoothly-notice",
 	styleUrl: "style.css",
 	scoped: true,
 })
-export class NoticeDisplay {
+export class SmoothlyNotice {
 	@Prop() notice: string | Notice = { type: "warning", message: "An error occured."}
 	@Prop({ mutable: true }) show: boolean = false
 	@Listen("trigger")
@@ -23,12 +23,11 @@ export class NoticeDisplay {
 		const notice = typeof(this.notice) == "string" ? JSON.parse(this.notice) as Notice : this.notice
 		const noticeColor = notice.type != "default" ? notice.type : "primary"
 		return !this.show ? <slot></slot> :
-			<div>
-				<slot></slot>
-				<div class={ notice.type }>
-					<span>{ notice.message }</span>
-					<smoothly-trigger color="dark" fill="clear" name="close"><smoothly-icon name="close-circle" color={ noticeColor }></smoothly-icon></smoothly-trigger>
-				</div>
-			</div>
+		[
+			<slot></slot>,
+			<aside class={ notice.type }>
+				<p>{ notice.message }<smoothly-trigger color="dark" fill="clear" name="close"><smoothly-icon name="close-circle" color={ noticeColor }></smoothly-icon></smoothly-trigger></p>
+			</aside>,
+		]
 	}
 }
