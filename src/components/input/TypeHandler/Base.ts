6ffb869758e2f0b7d5ec6def8601a01272311a13
Gabriel Nilsson <gabriel.nilsson@payfunc.com>
2019-07-24 14:58:04 +0200

    Formatting bugfix

--- src/components/input/TypeHandler/Base.ts
+++ src/components/input/TypeHandler/Base.ts
@@ -52,12 +52,13 @@ export class Base extends TypeHandler {
 		return result
 	}
 	formatState(stateEditor: StateEditor): State {
-		let index = 0
-		while (stateEditor.value.length > index) {
-			let next = stateEditor.value[index]
-			next = this.format(next, index, stateEditor.value.substring(0, index))
-			stateEditor.replace(next, index, index + 1)
-			index += next.length
+		let editorIndex = 0
+		let beforeIndex = 0
+		const before = stateEditor.value
+		for (let next of before) {
+			next = this.format(next, beforeIndex++, stateEditor.value.substring(0, editorIndex))
+			stateEditor.replace(next, editorIndex, editorIndex + 1)
+			editorIndex += next.length
 		}
 		return stateEditor.stateCopy
 	}
