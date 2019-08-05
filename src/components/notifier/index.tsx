aef95c703c75b3ee14477c45a6d7c5f0f6947e30
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2019-08-05 14:03:59 +0200

    Notifiertimer (#42)
    * Added timer to notifier
    
    * Import ordering
    
    * Timeout instead of interval
    
    * More handling of timeouts

--- src/components/notifier/index.tsx
+++ src/components/notifier/index.tsx
@@ -1,5 +1,5 @@
 // tslint:disable-next-line: no-implicit-dependencies
-import { Component, Prop, h, Listen } from "@stencil/core"
+import { Component, Listen, Prop, Watch, h } from "@stencil/core"
 import { Trigger, Notice } from "smoothly-model"
 
 @Component({
@@ -8,12 +8,26 @@ import { Trigger, Notice } from "smoothly-model"
 	scoped: true,
 })
 export class Notifier {
+	private timer?: number
 	@Prop() notice?: string | Notice
 	@Listen("trigger")
 	onTrigger(event: CustomEvent<Trigger>) {
 		if (event.detail.name == "close") {
 			event.stopPropagation()
 			this.notice = undefined
+		}
+	}
+	@Watch("notice")
+	onUpdatedNotice(newValue: string | Notice) {
+		if (this.timer) {
+			window.clearTimeout(this.timer)
+			this.timer = undefined
+		}
+		if (newValue != undefined) {
+			this.timer = window.setTimeout(() => {
+				this.notice = undefined
+				this.timer = undefined
+			}, 5000)
 		}
 	}
 	@Listen("notice")
