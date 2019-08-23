a808c3bf2fe7b78730738ad5617c0683bb9c9e7b
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2019-08-23 13:23:49 +0200

    Urlencoded (#48)
    * Added components for urlencoded
    
    * Restored frame
    
    * Removed styleurl

---
+++ src/components/tuple/index.tsx
@@ -0,0 +1,16 @@
+// tslint:disable-next-line: no-implicit-dependencies
+import { Component, Prop, h } from "@stencil/core"
+
+@Component({
+	tag: "smoothly-tuple",
+})
+
+export class SmoothlyTuple {
+	@Prop() tuple: [string, string]
+	render() {
+		return [
+			<b>{ this.tuple[0] }</b>, ": ",
+			this.tuple[1], <br />,
+		]
+	}
+}
