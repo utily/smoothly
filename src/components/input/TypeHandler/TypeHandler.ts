6e7c38e2be229c1c2f2a8ea6c5201e528db0d739
gabrielonilsson <52487218+gabrielonilsson@users.noreply.github.com>
2019-11-18 13:45:21 +0100

    Removed blockNextSet from input (#75)

--- src/components/input/TypeHandler/TypeHandler.ts
+++ src/components/input/TypeHandler/TypeHandler.ts
@@ -6,23 +6,17 @@ import { StateEditor } from "../StateEditor"
 export abstract class TypeHandler {
 	get value(): any { return this.state.value }
 	set value(value: any) {
-		if (this.blockNextSet)
-			this.blockNextSet = false
-		else
-			this.state = { ...this.state, value }
+		this.state = { ...this.state, value }
 	}
 	get native(): Component<string> { return { ...this.component, value: this.state.value } }
-	private blockNextSet = false
 	private stateValue: Readonly<State> = { value: "", selectionStart: 0, selectionEnd: 0 }
 	private get state(): Readonly<State> { return this.stateValue }
 	private set state(state: Readonly<State>) {
 		const updateComponent = this.stateValue.value != state.value
 		if (updateComponent || this.stateValue.selectionStart != state.selectionStart || this.stateValue.selectionEnd != state.selectionEnd) {
 			this.stateValue = state
-			if (updateComponent) {
-				this.blockNextSet = true
+			if (updateComponent)
 				this.component.value = this.value
-			}
 		}
 	}
 	protected constructor(protected readonly component: Component<any>) {
