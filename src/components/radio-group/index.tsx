9f2686ab6b6e2558e0c9a734c989e496ba03aca9
Simon Mika <simonmika@users.noreply.github.com>
2019-11-18 14:24:01 +0100

    Added radio group. (#76)

---
+++ src/components/radio-group/index.tsx
@@ -0,0 +1,13 @@
+// tslint:disable-next-line: no-implicit-dependencies
+import { Component, Prop, h } from "@stencil/core"
+
+@Component({
+	tag: "smoothly-radio-group",
+	styleUrl: "style.css",
+})
+export class SmoothlyToggleGroup {
+	@Prop({ reflect: true }) orientation?: "horizontal" | "vertical" = "horizontal"
+	render() {
+		return <slot></slot>
+	}
+}
