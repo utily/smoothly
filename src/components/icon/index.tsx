7681389a7de2c8fe686629b65a215705ae7b2842
fredrikbryntesson <fredrikbryntesson@users.noreply.github.com>
2020-03-05 15:18:22 +0100

    Added tooltip to icon (#103)

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -12,13 +12,14 @@ export class SmoothlyIcon {
 	@Prop({ reflectToAttr: true }) fill: Fill = "solid"
 	@Prop() name?: string
 	@Prop({ reflectToAttr: true }) size: "small" | "medium" | "large" = "medium"
+	@Prop() toolTip?: string
 	@State() document?: string
 	@Watch("name")
 	async loadDocument() {
 		if (this.name) {
 			const url = `https://unpkg.com/ionicons@5.0.0/dist/svg/${ this.name }.svg`
 			const response = await fetch(url)
-			this.document = response.ok ? await response.text() : undefined
+			this.document = response.ok ? (await response.text()).replace(/(<title>)[\w\d\s\-]*(<\/title>)/, `<title>${ this.toolTip || "" }</title>`) : undefined
 		}
 	}
 	async componentWillLoad() {
