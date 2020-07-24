5e2a053c3ee8d165d13090f89d43040b7a1a6b7f
Simon Mika <simon@mika.se>
2020-07-24 13:26:55 +0200

    Fully working demo.

--- src/components/app-demo/index.tsx
+++ src/components/app-demo/index.tsx
@@ -11,16 +11,21 @@ export class SmoothlyAppDemo {
 		return <smoothly-app>
 			<header>
 				<h1><stencil-route-link url="/">Smoothly App</stencil-route-link></h1>
+				<p>Center</p>
 				<nav>
 					<ul>
 						<li><stencil-route-link url="/input">Input</stencil-route-link></li>
+						<li><stencil-route-link url="/display">Display</stencil-route-link></li>
+						<li><stencil-route-link url="/select">Select</stencil-route-link></li>
 					</ul>
 				</nav>
 			</header>
 			<stencil-router titleSuffix=" - Smoothly App">
 				<stencil-route-switch>
-					<stencil-route url="/" component="smoothly-adresses" exact={ true } />
+					<stencil-route url="/" component="smoothly-login" exact={ true } />
 					<stencil-route url="/input" component="smoothly-input-demo" />
+					<stencil-route url="/display" component="smoothly-display-demo" />
+					<stencil-route url="/select" component="smoothly-select-demo" />
 				</stencil-route-switch>
 			</stencil-router>
 		</smoothly-app>
---
+++ src/components/display-demo/index.tsx
@@ -0,0 +1,27 @@
+// tslint:disable-next-line: no-implicit-dependencies
+import { Component, h } from "@stencil/core"
+@Component({
+	tag: "smoothly-display-demo",
+})
+export class SmoothlyDisplayDemo {
+	render() {
+		return [
+			<main>
+				<smoothly-display-date-time datetime="2019-08-15T08:08:17.65Z"></smoothly-display-date-time>
+				<dl>
+					<dt>text</dt><dd><smoothly-display type="text" value="text"></smoothly-display></dd>
+					<dt>postal code</dt><dd><smoothly-display type="postal-code" value="752 31"></smoothly-display></dd>
+					<dt>password</dt><dd><smoothly-display type="password" value="password"></smoothly-display></dd>
+					<dt>email</dt><dd><smoothly-display type="email" value="test@example.com"></smoothly-display></dd>
+					<dt>price</dt><dd><smoothly-display type="price" value="13.37" currency="SEK"></smoothly-display></dd>
+					<dt>percent</dt><dd><smoothly-display type="percent" value="42"></smoothly-display></dd>
+					<dt>phone</dt><dd><smoothly-display type="phone" value="0101881108"></smoothly-display></dd>
+					<dt>card number</dt><dd><smoothly-display type="card-number" value="4111111111111111"></smoothly-display></dd>
+					<dt>card expires</dt><dd><smoothly-display type="card-expires" value="7/22"></smoothly-display></dd>
+					<dt>card csc</dt><dd><smoothly-display type="card-csc" value="987"></smoothly-display></dd>
+				</dl>
+				<smoothly-urlencoded data="hej=hopp&tjena=moss"></smoothly-urlencoded>
+			</main>
+		]
+	}
+}
