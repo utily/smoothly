d60b50ad82ab2eb2d0123a12de29c97ea72e7d9d
Simon Mika <simonmika@users.noreply.github.com>
2020-08-24 16:47:59 +0200

    Route fix (#148)
    * Fixed routing.
    
    * Code cleanup.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,61 +1,96 @@
-import { FunctionalComponent, h } from "@stencil/core"
+import { ChildNode, FunctionalComponent, h, VNode } from "@stencil/core"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createStaticRouter, Route, href } from "stencil-router-v2"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Router = createStaticRouter()
 
-export const App: FunctionalComponent<{ label: string }> = (attributes, children, utils) => (
-	<smoothly-app>
-		<header>
-			<h1>
-				<a {...href(resolve("") ?? "/")}>{attributes.label}</a>
-			</h1>
-			{children.filter(child => child.$attrs$.slot == "header")}
-			<nav>
-				<ul>
-					{[
-						...children.filter(child => child.$attrs$.slot == "nav-start"),
-						...children
-							.map(child => [child.$attrs$.label, child.$attrs$.path])
-							.filter(e => e[0] && e[1])
-							.map(e => <a href={e[1]}>{e[0]}</a>),
-						...children.filter(child => child.$attrs$.slot == "nav-end"),
-					]
-						.map(e => {
-							const url = resolve(e.$attrs$.href)
-							return e.$tag$ != "a" ? (
-								e
-							) : url ? (
-								<a {...href(url)} class={Router.activePath == url ? "active" : ""}>
-									{e.$children$}
-								</a>
-							) : (
-								<a target="new" {...e.$attrs$}>
-									{e.$children$}
-								</a>
+export const App: FunctionalComponent<{ label: string }> = (attributes, nodes, utils) => {
+	const emptyNode = Object.entries(nodes[0]).reduce<{ [property: string]: any }>((r, entry) => {
+		r[entry[0]] = null
+		return r
+	}, {}) as VNode
+	const emptyChild = nodeToChild(emptyNode)
+	function nodeToChild(node: VNode): ChildNode & { node: VNode } {
+		let result: (ChildNode & { node: VNode }) | undefined
+		utils.forEach([node], (child, index) => (result = { ...child, node: nodes[index] }))
+		return result ?? emptyChild
+	}
+	function childToNode(child: ChildNode): VNode {
+		return utils.map([emptyNode], c => ({ ...c, ...child }))[0]
+	}
+	const children = nodes.map(nodeToChild)
+	return (
+		<smoothly-app>
+			<header>
+				<h1>
+					<a {...href(resolve("") ?? "/")}>{attributes.label}</a>
+				</h1>
+				{children.filter(child => child.vattrs.slot == "header").map(child => child.node)}
+				<nav>
+					<ul>
+						{utils
+							.map(
+								[
+									...children.filter(child => child.vattrs.slot == "nav-start"),
+									...children.filter(child => child.vattrs.label && child.vattrs.path),
+									...children.filter(child => child.vattrs.slot == "nav-end"),
+								].map(child => child.node),
+								child => {
+									if (child.vattrs.label && child.vattrs.path)
+										child = {
+											...emptyChild,
+											vtag: "a",
+											vattrs: { href: child.vattrs.path },
+											vchildren: [childToNode({ vtext: child.vattrs.label })],
+										}
+									const url = resolve(child.vattrs.href)
+									return child.vtag != "a"
+										? child
+										: url
+										? {
+												...child,
+												vattrs: {
+													...child.vattrs,
+													class: [child.vattrs.class, Router.activePath == url ? "active" : ""].join(" ") || undefined,
+													href: url,
+												},
+										  }
+										: { ...child, vattrs: { ...child.vattrs, target: "new" } }
+								}
 							)
-						})
-						.map(e => (
-							<li>{e}</li>
-						))}
-				</ul>
-			</nav>
-		</header>
-		<content>
-			<Router.Switch>
-				{children
-					.filter(child => child.$attrs$.path != undefined)
-					.map(child =>
-						child.$attrs$.to ? (
-							<Route path={resolve(child.$attrs$.path) ?? child.$attrs$.path} to={child.$attrs$.to}></Route>
-						) : (
-							<Route path={resolve(child.$attrs$.path) ?? child.$attrs$.path}>{child}</Route>
-						)
-					)}
-			</Router.Switch>
-		</content>
-	</smoothly-app>
-)
+							.map(node => {
+								const child = nodeToChild(node)
+								return child.vtag == "a" && !child.vattrs.target ? (
+									<a {...child.vattrs} {...href(child.vattrs.href)}>
+										{child.vchildren}
+									</a>
+								) : (
+									child.node
+								)
+							})
+							.map(child => (
+								<li>{child}</li>
+							))}
+					</ul>
+				</nav>
+			</header>
+			<content>
+				<Router.Switch>
+					{children
+						.filter(child => child.vattrs.path != undefined)
+						.map(child =>
+							child.vattrs.to ? (
+								<Route path={resolve(child.vattrs.path) ?? child.vattrs.path} to={child.vattrs.to}></Route>
+							) : (
+								<Route path={resolve(child.vattrs.path) ?? child.vattrs.path}>{child.node}</Route>
+							)
+						)}
+				</Router.Switch>
+			</content>
+		</smoothly-app>
+	)
+}
 function resolve(url: string): string | undefined {
 	const u = new URL(url, document.baseURI)
 	return url == ""
