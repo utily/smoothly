a0d9a693bd1dbd8d9f2ec8fa3a3dd36122a7b524
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2021-03-05 13:25:15 +0100

    Enables setting of message origin for iframes (#176)

--- src/components/frame/index.tsx
+++ src/components/frame/index.tsx
@@ -17,14 +17,17 @@ export class SmoothlyFrame {
 			this.element && this.element.firstElementChild ? (this.element.firstElementChild as HTMLIFrameElement) : undefined
 		return (iframe && iframe.contentWindow) || undefined
 	}
-	get urlOrigin(): string {
+	@Prop()
+	origin: string | undefined
+
+	get defaultOrigin(): string {
 		const match = this.url.match(/^(([a-z]+\+)*[a-z]+:\/\/)?[^/^\n]+/)
 		return match ? match[0] : "*"
 	}
 	componentDidLoad() {
 		if (this.contentWindow)
 			Message.listen(
-				this.urlOrigin,
+				this.origin ?? this.defaultOrigin,
 				(destination: string, content: any) => {
 					if (destination == this.name)
 						if (Trigger.is(content))
@@ -40,10 +43,10 @@ export class SmoothlyFrame {
 	@Method()
 	async send(message: string | Message<any>, content?: Trigger | any): Promise<void> {
 		if (typeof message == "string")
-			Message.send(this.urlOrigin + "#" + message, content, this.contentWindow)
+			Message.send((this.origin ?? this.defaultOrigin) + "#" + message, content, this.contentWindow)
 		else if (Message.is(message) && this.contentWindow)
 			Message.send(
-				{ destination: this.urlOrigin + "#" + message.destination, content: message.destination },
+				{ destination: (this.origin ?? this.defaultOrigin) + "#" + message.destination, content: message.destination },
 				this.contentWindow
 			)
 	}
