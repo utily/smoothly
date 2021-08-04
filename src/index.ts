1ea010a2caa7be29498d515a4c66d176b58668f6
Magnus Gustafsson <magnus.ja.gustafsson@gmail.com>
2021-08-04 10:34:44 +0200

    Add to comma rgb (#217)
    Co-authored-by: Magnus Gustafsson <magnus.gustafsson@intergiro.com>

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Components, JSX } from "./components"
 export { App } from "./components/App"
 export { Autocomplete, ClientIdentifier, Color, Expand, Fill, Message, Notice, Trigger, OptionType } from "./model"
+export { toCommaRgb } from "./utilities"
