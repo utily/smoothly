22ef009dd37547bbd1d43c02a790c344a264fa3d
Qijie Yu <qijie@issuefab.com>
2021-08-06 17:06:28 +0200

    Fixed intialization issue in item.

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Event, EventEmitter, h, Listen, Method, Prop } from "@stencil/core"
+import { Component, Element, Event, EventEmitter, h, Listen, Method, Prop, Watch } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-item",
@@ -10,11 +10,19 @@ export class Item {
 	@Prop() value: any
 	@Prop({ reflect: true, mutable: true }) selected: boolean
 	@Event() itemSelected: EventEmitter<void>
-
+	@Watch("selected")
+	onSelectedChanged(value: boolean, old: boolean) {
+		if (value && !old)
+			this.itemSelected.emit()
+	}
 	@Listen("click")
 	onClick() {
 		this.selected = true
 		this.itemSelected.emit()
+	}
+	componentDidLoad() {
+		if (this.selected)
+			this.itemSelected.emit()
 	}
 	@Method()
 	async filter(filter: string): Promise<boolean> {
