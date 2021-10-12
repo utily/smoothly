591c7890e5ad0c5f71a0375086452506f8a22d81
Qijie Yu <qijie@issuefab.com>
2021-10-12 10:36:37 +0200

    Keep less change.

--- src/components/table/demo/index.tsx
+++ src/components/table/demo/index.tsx
@@ -7,22 +7,47 @@ import { Component, h } from "@stencil/core"
 })
 export class TableDemo {
 	render() {
-		return (
+		return [
 			<smoothly-table>
 				<smoothly-table-row>
 					<smoothly-table-header>Header A</smoothly-table-header>
 					<smoothly-table-header>Header B</smoothly-table-header>
 					<smoothly-table-header>Header C</smoothly-table-header>
 				</smoothly-table-row>
-				<smoothly-table-row>
-					<smoothly-table-cell>Value 1A</smoothly-table-cell>
-					<smoothly-table-cell>
+				<smoothly-table-expandable-row>
+					<smoothly-table-expandable-cell>
+						Value 1A
+						<div slot="detail">1A details</div>
+					</smoothly-table-expandable-cell>
+					<smoothly-table-expandable-cell>
 						<smoothly-display type="price" value={20} currency="EUR"></smoothly-display>
-					</smoothly-table-cell>
-					<smoothly-table-cell>
+						<div slot="detail">Budget details.</div>
+					</smoothly-table-expandable-cell>
+					<smoothly-table-expandable-cell>
 						<smoothly-display type="price" value={18} currency="EUR"></smoothly-display>
-					</smoothly-table-cell>
-					<div slot="detail">Cost details.</div>
+						<div slot="detail">Cost details.</div>
+					</smoothly-table-expandable-cell>
+				</smoothly-table-expandable-row>
+				<smoothly-table-expandable-row>
+					<smoothly-table-expandable-cell>
+						Value 1A
+						<div slot="detail">2A details</div>
+					</smoothly-table-expandable-cell>
+					<smoothly-table-expandable-cell>
+						<smoothly-display type="price" value={30} currency="EUR"></smoothly-display>
+						<div slot="detail">Budget details.</div>
+					</smoothly-table-expandable-cell>
+					<smoothly-table-expandable-cell>
+						<smoothly-display type="price" value={38} currency="EUR"></smoothly-display>
+						<div slot="detail">Cost details.</div>
+					</smoothly-table-expandable-cell>
+				</smoothly-table-expandable-row>
+			</smoothly-table>,
+			<smoothly-table>
+				<smoothly-table-row>
+					<smoothly-table-header>Header D</smoothly-table-header>
+					<smoothly-table-header>Header E</smoothly-table-header>
+					<smoothly-table-header>Header F</smoothly-table-header>
 				</smoothly-table-row>
 				<smoothly-table-row>
 					<smoothly-table-cell>Value 1A</smoothly-table-cell>
@@ -34,7 +59,17 @@ export class TableDemo {
 					</smoothly-table-cell>
 					<div slot="detail">Cost details.</div>
 				</smoothly-table-row>
-			</smoothly-table>
-		)
+				<smoothly-table-row>
+					<smoothly-table-cell>Value 1A</smoothly-table-cell>
+					<smoothly-table-cell>
+						<smoothly-display type="price" value={20} currency="EUR"></smoothly-display>
+					</smoothly-table-cell>
+					<smoothly-table-cell>
+						<smoothly-display type="price" value={18} currency="EUR"></smoothly-display>
+					</smoothly-table-cell>
+					<div slot="detail">Cost details.</div>
+				</smoothly-table-row>
+			</smoothly-table>,
+		]
 	}
 }
