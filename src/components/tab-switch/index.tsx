6ac4b5d34410425a1e116075a1fd19c205e4bc35
Qijie Yu <qijie@issuefab.com>
2021-10-29 16:54:10 +0200

    Click different tab can switch to differet div.

---
+++ src/components/tab-switch/index.tsx
@@ -0,0 +1,30 @@
+import { Component, Element, h, Host, Listen, State, Watch } from "@stencil/core"
+
+@Component({
+	tag: "smoothly-tab-switch",
+	styleUrl: "style.css",
+	scoped: true,
+})
+export class SmoothlyTabSwitch {
+	@Element() element: HTMLSmoothlyTabSwitchElement
+	@State() selectedElement: HTMLSmoothlyTabElement
+	@Listen("expansionOpen")
+	openChanged(event: CustomEvent) {
+		this.selectedElement = event.target as HTMLSmoothlyTabElement
+		this.selectedElement.open = true
+		this.element.after(event.detail)
+	}
+	@Watch("selectedElement")
+	onSelectedChange(value: HTMLSmoothlyTabElement, old: HTMLSmoothlyTabElement) {
+		if (old)
+			old.open = false
+	}
+
+	render() {
+		return (
+			<Host>
+				<slot></slot>
+			</Host>
+		)
+	}
+}
