0d5a5457a3a5e03dba34179c69eff42b31572b91
Qijie Yu <qijie@issuefab.com>
2021-11-29 13:19:46 +0100

    Delete console.log

--- src/components/calendar/generate.ts
+++ src/components/calendar/generate.ts
@@ -40,7 +40,6 @@ export function months(current: isoly.Date): { date: isoly.Date; name: string; s
 			name: day.toLocaleString(undefined, { month: "long" }),
 			selected: date.substr(0, 7) == current.substr(0, 7),
 		})
-		console.log("date?", date)
 	}
 	return result
 }
