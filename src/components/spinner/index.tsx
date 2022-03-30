6e0a1ee9808b430aaa7975e4dbf0ad56e30e1056
Magnus Gustafsson <magnus@intergiro.com>
2022-03-30 18:09:34 +0200

    Add size prop on spinner

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,13 +1,33 @@
-import { Component, h, Prop } from "@stencil/core"
+import { Component, h, Host, Prop } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-spinner",
-	styleUrl: "style.css",
+	styleUrl: "style.scss",
 	scoped: true,
 })
 export class SmoothlySpinner {
 	@Prop({ reflect: true }) active: boolean
+	@Prop({ reflect: true }) size: "small" | "medium" | "large" = "large"
+
 	render() {
-		return <div>Loading...</div>
+		const strokeWidth = this.size == "large" ? 6 : this.size == "medium" ? 8 : 12
+		return (
+			<Host
+				style={{
+					"--color": `var(--spinner-color)`,
+					"--size": this.size == "large" ? "5em" : this.size == "medium" ? "3em" : "1.2em",
+				}}>
+				<svg class="spinner" viewBox={`0 0 ${60 + strokeWidth} ${60 + strokeWidth}`} xmlns="http://www.w3.org/2000/svg">
+					<circle
+						class="path"
+						fill="none"
+						stroke-width={strokeWidth}
+						stroke-linecap="round"
+						cx={`${30 + strokeWidth / 2}`}
+						cy={`${30 + strokeWidth / 2}`}
+						r="30"></circle>
+				</svg>
+			</Host>
+		)
 	}
 }
