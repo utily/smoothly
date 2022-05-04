2398035f4c26a225e9209f69565e2b138cf7f435
Robin Nedergård <robin.nedergard@payfunc.com>
2022-05-04 16:09:10 +0200

    Working svg element

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -1,12 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Component, h, Host, Prop } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-svg",
-	styleUrl: "style.scss",
+	styleUrl: "style.css",
+	scoped: true,
 })
 export class SmoothlySvg {
 	@Prop({ reflect: true }) url!: string
 	@Prop({ reflect: true }) size?: "large" | "medium" | "small" | "tiny" | { height: string; width: string }
+	@Prop() color: string
 	private readonly defaultSizes: Record<"large" | "medium" | "small" | "tiny", { height: string; width: string }> = {
 		large: { height: "50rem", width: "50rem" },
 		medium: { height: "30rem", width: "30rem" },
@@ -18,17 +21,20 @@ export class SmoothlySvg {
 			? typeof this.size == "object"
 				? this.size.height
 				: this.defaultSizes[this.size].height
-			: "50em"
+			: "10rem"
 		const width = this.size
 			? typeof this.size == "object"
 				? this.size.width
 				: this.defaultSizes[this.size].width
-			: "50em"
+			: "10rem"
 		return (
 			<Host
 				style={{
+					"min-height": height,
+					"min-width": width,
 					"--size-height": height,
 					"--size-width": width,
+					"--color": this.color,
 				}}>
 				<object height={height} width={width} type="image/svg+xml" data={this.url} />
 			</Host>
