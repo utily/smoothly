92011af6512be84978d69e441edaa0258a557fd9
Linus Johansson <linus@issuefab.com>
2022-06-15 11:55:26 +0200

    removed render sort icon in smoothly-table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Event, EventEmitter, h, Listen } from "@stencil/core"
+import { Component, Element, h } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-table",
@@ -7,14 +7,6 @@ import { Component, Element, Event, EventEmitter, h, Listen } from "@stencil/cor
 })
 export class Table {
 	@Element() element: HTMLSmoothlyTableElement
-	@Event() loadMore: EventEmitter<void>
-	sortedOn?: HTMLSmoothlyTableHeaderElement
-	@Listen("sort")
-	onSort(event: CustomEvent<{ property: string; direction: "ascending" | "descending" }>) {
-		if (this.sortedOn && this.sortedOn != event.target)
-			this.sortedOn.sortDirection = undefined
-		this.sortedOn = event.target as HTMLSmoothlyTableHeaderElement
-	}
 	render() {
 		return [<slot></slot>]
 	}
