0eb1bcc908d4ff389febb4b7486f343164d69862
Linus Johansson <linus@issuefab.com>
2022-06-23 15:36:52 +0200

    expandable row opens the same slot

--- src/components/table/expandable/row/index.tsx
+++ src/components/table/expandable/row/index.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Listen, Prop } from "@stencil/core"
+import { Component, Element, Event, EventEmitter, h, Host, Listen, Prop, State, Watch } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-table-expandable-row",
@@ -6,24 +6,44 @@ import { Component, h, Host, Listen, Prop } from "@stencil/core"
 	scoped: true,
 })
 export class TableExpandableRow {
+	@Element() element: HTMLSmoothlyTableRowElement
+	expansionElement?: HTMLTableRowElement
+	@Event() expansionOpen: EventEmitter<HTMLElement>
+	@Prop() align: "left" | "center" | "right" = "left"
 	@Prop({ mutable: true, reflect: true }) open: boolean
+	@State() beginOpen: boolean
+	@Watch("open")
+	openChanged(value: boolean) {
+		if (this.expansionElement)
+			this.element.after(this.expansionElement)
+	}
+
 	@Listen("click")
-	onClick() {
+	onClick(e: UIEvent) {
 		this.open = !this.open
+		e.stopPropagation()
+	}
+
+	componentDidRender(): void {
+		this.expansionOpen.emit(this.expansionElement)
+		if (this.expansionElement && this.open)
+			this.element.after(this.expansionElement)
 	}
 	render() {
-		return [
-			<Host>
-				<tr>
-					<slot></slot>
-				</tr>
-				<tr>
+		return (
+			<Host style={{ textAlign: this.align }}>
+				<slot></slot>
+				<smoothly-table-cell>
+					<smoothly-icon name="chevron-forward" size="tiny"></smoothly-icon>
+				</smoothly-table-cell>
+				<tr ref={e => (this.expansionElement = e)}>
 					<td colSpan={500} class={!this.open ? "hide" : ""}>
-						<slot name="detail"></slot>
+						<div class="slot-detail">
+							<slot name="detail"></slot>
+						</div>
 					</td>
 				</tr>
-				,
-			</Host>,
-		]
+			</Host>
+		)
 	}
 }
