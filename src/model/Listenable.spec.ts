2f64e6c2bfbd4d3970adb205dced0e285e994a3b
Elias Eriksson <eliaseriksson95@gmail.com>
2022-08-23 11:28:12 +0200

    Listenable aspect (#316)
    * type fix + test
    
    * implemented remove listener
    
    * all tests use State
    
    * removeListener -> unlusten
    
    * removed unnecessary TS type cast

---
+++ src/model/Listenable.spec.ts
@@ -0,0 +1,57 @@
+import { Listenable } from "./Listenable"
+
+describe("Listenable", () => {
+	class State {
+		disabled = false
+	}
+	it("load", () => {
+		const state = Listenable.load(new State())
+		expect(typeof state.listen).toEqual("function")
+		expect(typeof state.unlisten).toEqual("function")
+		expect(state.disabled).toEqual(false)
+	})
+	it("subscribe", () => {
+		const state = Listenable.load(new State())
+		class FakeComponent {
+			disabled: boolean
+			constructor() {
+				this.componentWillLoad()
+			}
+			componentWillLoad() {
+				state.listen("disabled", disabled => (this.disabled = disabled))
+			}
+		}
+		const components = [new FakeComponent(), new FakeComponent()]
+		expect(components.every(component => !component.disabled))
+		state.disabled = true
+		expect(components.every(component => component.disabled)).toEqual(true)
+	})
+	it("unsubscribe", () => {
+		const state = Listenable.load(new State())
+		let count = 0
+		class FakeComponent {
+			disabled: boolean
+			constructor() {
+				this.subscribe = this.subscribe.bind(this)
+				this.componentWillLoad()
+			}
+			subscribe(disabled: boolean) {
+				count += 1
+				this.disabled = disabled
+			}
+			componentWillLoad() {
+				state.listen("disabled", this.subscribe)
+			}
+			disconnectedCallback() {
+				state.unlisten("disabled", this.subscribe)
+			}
+		}
+		const component = new FakeComponent()
+		expect(count).toEqual(1)
+		state.disabled = true
+		expect(count).toEqual(2)
+		component.disconnectedCallback()
+		state.disabled = false
+		expect(count).toEqual(2)
+	})
+})
---
+++ src/model/Listenable.ts
@@ -0,0 +1,34 @@
+export class Listenable<T extends Record<string, any>> {
+	#listeners: Listeners<T> = {}
+	listen<K extends keyof T>(this: T & Listenable<T>, property: K, listener: Listener<T[K]>): void {
+		this.#listeners[property]?.push(listener) ?? (this.#listeners[property] = [listener])
+		listener(this[property])
+	}
+	unlisten<K extends keyof T>(property: K, listener: Listener<T[K]>): void {
+		const index = this.#listeners[property]?.indexOf(listener)
+		index != undefined && index >= 0 && this.#listeners[property]?.splice(index, 1)
+	}
+
+	static load<T>(backend: T): T & Listenable<T> {
+		const result = new Listenable()
+		for (const property in backend) {
+			if (Object.prototype.hasOwnProperty.call(backend, property)) {
+				Object.defineProperty(result, property, {
+					get() {
+						return backend[property]
+					},
+					set(value) {
+						backend[property] = value
+						result.#listeners[property]?.forEach(listener => listener(value))
+					},
+				})
+			}
+		}
+		return result as T & Listenable<T>
+	}
+}
+
+export type Listener<V> = (value: V) => void
+export type Listeners<T> = {
+	[K in keyof T]?: Listener<T[K]>[]
+}
