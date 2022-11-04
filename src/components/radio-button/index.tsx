543d3392e8a4cac58691465c7d4acfd9c31bd0d1
Ztawh <amandahwatz@gmail.com>
2022-11-04 11:12:01 +0100

    Change what the radio button returns

--- src/components/radio-button/index.tsx
+++ src/components/radio-button/index.tsx
@@ -7,18 +7,22 @@ import { Selected } from "./Selected"
 	scoped: true,
 })
 export class SmoothlyRadioButton {
+	@Prop() name?: string
 	@Prop({ mutable: true }) value?: any
 	@Prop({ reflect: true }) deselectable?: boolean
 	@Prop({ reflect: true }) decoration: "button" | "radio" = "radio"
 	active?: Selected
-	@Event() radioButtonSelected: EventEmitter<any>
+	@Event() radioButtonSelected: EventEmitter<{ name: string | undefined; value: any }>
 
 	@Listen("radioItemSelectInternal", { capture: true })
 	radioSelectHandler(event: CustomEvent<Selected>) {
 		if (this.deselectable || this.active?.value != event.detail.value) {
 			this.active?.select(false)
 			this.active = event.detail
-			this.radioButtonSelected.emit((this.value = this.active.value))
+			this.radioButtonSelected.emit({
+				name: this.name,
+				value: (this.value = this.active.selected ? this.active.value : undefined),
+			})
 			this.active.select(this.active.selected)
 		}
 	}
