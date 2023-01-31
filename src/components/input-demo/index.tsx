5e0bf96e50ac870e8f43477c4fd0d99f2eb4affc
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2023-01-31 11:00:53 +0100

    Changed selector on radio-button.

--- src/components/input-demo/index.tsx
+++ src/components/input-demo/index.tsx
@@ -80,11 +80,9 @@ export class SmoothlyInputDemo {
 						<h2>Smoothly Radio</h2>
 						<h3>Deselectable Radio List with button</h3>
 						<smoothly-radio-button deselectable decoration="button">
-							<smoothly-radio-button-item value={{ some: "content", yes: "sir" }}>
+							<smoothly-radio-button-item value={{ some: "content", yes: "sir" }} color="primary">
 								<span>Option 1</span>
-								<span slot="expansion" color="primary">
-									Some description.
-								</span>
+								<span slot="expansion">Some description.</span>
 							</smoothly-radio-button-item>
 							<smoothly-radio-button-item value={{ some: "thing", yes: "miss" }}>
 								<span>Option 2</span>
@@ -96,11 +94,9 @@ export class SmoothlyInputDemo {
 						</smoothly-radio-button>
 						<h3>Radio List with button</h3>
 						<smoothly-radio-button decoration="button">
-							<smoothly-radio-button-item value={{ some: "content", yes: "sir" }}>
+							<smoothly-radio-button-item value={{ some: "content", yes: "sir" }} color="warning">
 								<span>Option 1</span>
-								<span slot="expansion" color="warning">
-									Some description.
-								</span>
+								<span slot="expansion">Some description.</span>
 							</smoothly-radio-button-item>
 							<smoothly-radio-button-item value={{ some: "thing", yes: "miss" }}>
 								<span>Option 2</span>
