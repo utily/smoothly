fa1ae724ffd12320316238c0f8f5e59c18c49bd9
Jieun Chung <jieun.chung@paxport.net>
2023-01-31 15:26:08 +0100

    render table

--- src/components/table/demo/checked/index.tsx
+++ src/components/table/demo/checked/index.tsx
@@ -1,29 +1,48 @@
-import { Component, h } from "@stencil/core"
+import { Component, ComponentWillLoad, h, State } from "@stencil/core"
+import * as http from "cloudly-http"
+import { Root } from "../filtered/Root"
 
 @Component({
 	tag: "smoothly-table-demo-checked",
 	styleUrl: "style.css",
 	scoped: true,
 })
-export class TableDemoChecked {
+export class TableDemoChecked implements ComponentWillLoad {
+	@State() data?: Root | false
+
+	async componentWillLoad(): Promise<void> {
+		const response = await http.fetch("https://catfact.ninja/breeds?limit=10")
+		this.data = response.status == 200 && (await response.body)
+	}
+
 	render() {
-		return [
-			<div>sketch</div>,
-			<br />,
-			<smoothly-table>
-				<smoothly-table-row>
-					<smoothly-table-header>
-						<smoothly-checkbox />
-					</smoothly-table-header>
-					<smoothly-table-header>header</smoothly-table-header>
-				</smoothly-table-row>
-				<smoothly-table-row>
-					<smoothly-table-cell>
-						<smoothly-checkbox />
-					</smoothly-table-cell>
-					<smoothly-table-cell>content</smoothly-table-cell>
-				</smoothly-table-row>
-			</smoothly-table>,
-		]
+		const data = this.data
+		return !data
+			? "Failed to load data."
+			: [
+					<div>sketch</div>,
+					<br />,
+					<smoothly-table>
+						<smoothly-table-row>
+							<smoothly-table-header>
+								<smoothly-checkbox />
+							</smoothly-table-header>
+							<smoothly-table-header>Breed</smoothly-table-header>
+							<smoothly-table-header>Coat</smoothly-table-header>
+						</smoothly-table-row>
+						{data.data.map((cat, idx) => {
+							console.log("breed", cat.breed, idx)
+							return (
+								<smoothly-table-row>
+									<smoothly-table-cell>
+										<smoothly-checkbox keys={cat.breed} index={idx}></smoothly-checkbox>
+									</smoothly-table-cell>
+									<smoothly-table-cell>{cat.breed}</smoothly-table-cell>
+									<smoothly-table-cell>{cat.coat}</smoothly-table-cell>
+								</smoothly-table-row>
+							)
+						})}
+					</smoothly-table>,
+			  ]
 	}
 }
