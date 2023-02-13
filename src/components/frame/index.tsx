02fcfac13ab9c747dbc8f53956fb1240efa218d5
Asima-Asja Salihovic <asima-asja.salihovic@pax2pay.com>
2023-02-13 17:30:28 +0100

    Build fix

--- src/components/frame/index.tsx
+++ src/components/frame/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Event, EventEmitter, h, Method, Prop } from "@stencil/core"
+import { Component, Element, Event, EventEmitter, Method, Prop } from "@stencil/core"
 import { Message, Trigger } from "../../model"
 
 @Component({
