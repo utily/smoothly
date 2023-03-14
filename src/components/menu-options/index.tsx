84d49a506a65ce1399e319470e197277df63c6fc
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-03-14 11:59:11 +0100

    fix: option for disabling select all (#395)

--- src/components/menu-options/index.tsx
+++ src/components/menu-options/index.tsx
@@ -116,7 +116,7 @@ export class SmoothlyMenuOptions {
 					this.filteredOptions.map((option, index) => (
 						<smoothly-option
 							style={this.optionStyle}
-							toggle={this.toggle}
+							toggle={option.toggle ?? this.toggle}
 							ref={el => {
 								index == 0 && (this.firstOptionsElement = el ?? this.firstOptionsElement)
 								el && (this.optionElements[index] = el)
