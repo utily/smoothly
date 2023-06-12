778f008960eceec8f1bcb0f85ce4607eb72b108a
Anton Kroner <anton.kroner@pax2pay.com>
2023-06-12 09:29:21 +0200

    Fixed export method.

---
+++ src/smoothly.ts
@@ -0,0 +1,20 @@
+export { Components, JSX } from "./components"
+export {
+	Autocomplete,
+	ClientIdentifier,
+	Color,
+	Fill,
+	GoogleFont,
+	Message,
+	Notice,
+	Trigger,
+	Option,
+	Listenable,
+	HasListenable,
+	WithListenable,
+	CanBeListenable,
+	StateBase,
+	Data,
+	redirect,
+} from "./model"
+export { Cosmetic } from "./utilities"
