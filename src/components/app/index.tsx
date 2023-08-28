5db09f8dcc31d1bff4d992e28e2c26fc36465ad6
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-28 11:05:47 +0200

    more specific selector

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -67,7 +67,7 @@ export class SmoothlyApp {
 						<a href={""}>{this.label}</a>
 					</h1>
 					<slot name="header"></slot>
-					<nav class={{ menuOpen: this.menuOpen }}>
+					<nav class={{ "menu-open": this.menuOpen }}>
 						<ul>
 							<slot name="nav-start"></slot>
 							<slot> </slot>
