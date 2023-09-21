68bb634a5d19d1b3ad9712955d572166a27acb7b
linusarnejohansson <86045901+linusarnejohansson@users.noreply.github.com>
2023-09-21 10:51:13 +0200

    spelling fix (#493)

--- src/components/input/Submitable.ts
+++
@@ -1,8 +0,0 @@
-export interface Submitable {
-	submit(): Promise<void>
-}
-export namespace Submitable {
-	export function is(value: Submitable | any): value is Submitable {
-		return value && typeof value == "object" && typeof value.submit == "function"
-	}
-}
