46d2a181a2b0d6f8304776db0bbfd1063bec7ca9
Linus Johansson <linus@issuefab.com>
2023-09-21 13:40:42 +0200

    change interface Input to include color?: Color

--- src/components/input/Input.ts
+++ src/components/input/Input.ts
@@ -1,9 +1,11 @@
 import { EventEmitter } from "@stencil/core"
+import { Color } from "../../model"
 import { Looks } from "./Looks"
 
 export interface Input {
+	color?: Color
 	name: string
 	looks: Looks
 	smoothlyInput: EventEmitter<Record<number, any>>
-	smoothlyInputLooks: EventEmitter<(looks: Looks) => void>
+	smoothlyInputLooks: EventEmitter<(looks: Looks, color: Color | undefined) => void>
 }
