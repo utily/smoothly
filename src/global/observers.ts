b2f3ce765e27971806ed44b75fbfc67c43daa950
Elias Eriksson <EliasEriksson95@gmail.com>
2023-11-03 14:25:02 +0100

    updated return type to closer mimic the interface of the WeakMap

--- src/global/observers.ts
+++ src/global/observers.ts
@@ -3,9 +3,9 @@ export namespace Observers {
 	export function get(...parameters: Parameters<typeof observers.get>): ReturnType<typeof observers.get> {
 		return observers.get(...parameters)
 	}
-	export function set(...parameters: Parameters<typeof observers.set>): IntersectionObserver {
+	export function set(...parameters: Parameters<typeof observers.set>): typeof Observers {
 		observers.set(...parameters)
-		return parameters[1]
+		return Observers
 	}
 	export function remove(...parameters: Parameters<typeof observers.delete>): ReturnType<typeof observers.delete> {
 		return observers.delete(...parameters)
