eb4a827fd2d5fc0fbe0c48efc34182b416675387
Emilia By <emilia.by@pax2pay.com>
2024-01-12 12:56:55 +0100

    Restructured icon loadDocument function.

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -10,35 +10,35 @@ import { Icon } from "../../model"
 export class SmoothlyIcon {
 	@Prop({ reflect: true }) color: Color
 	@Prop({ reflect: true }) fill: Fill = "solid"
-	@Prop({ reflect: true }) name?: Icon | "empty"
+	@Prop({ reflect: true }) name: Icon | "empty" = "empty"
 	@Prop({ reflect: true }) size: "tiny" | "small" | "medium" | "large" = "medium"
 	@Prop() toolTip?: string
 	@State() document?: string
 	@Watch("name")
-	async loadDocument() {
-		if (this.name)
-			this.document =
-				this.name != "empty"
-					? (await Icon.load(this.name))?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
-					: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-			<title>Empty</title>
-			</svg>`
+	async loadDocument(): Promise<string> {
+		let result: string | undefined
+		if (this.name != "empty") {
+			result = await Icon.load(this.name)
+			result = result
+				?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
+				.replace(` width="512" height="512"`, "")
+				.replace(/stroke:#000;/gi, "")
+			if (result?.includes("<title>"))
+				result = result.replace(/(<title>)[\w\d\s-]*(<\/title>)/, `<title>${this.toolTip || ""}</title>`)
+			else
+				result = result?.replace(/(.*>)(<\/svg>$)/, `$1<title>${this.toolTip || ""}</title>$2`)
+		}
+		return (
+			result ??
+			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+		<title>Empty</title>
+		</svg>`
+		)
 	}
 	async componentWillLoad() {
-		this.loadDocument()
+		this.document = await this.loadDocument()
 	}
 	render() {
-		return (
-			<Host
-				innerHTML={
-					this.document
-						? this.document
-								.replace(` width="512" height="512"`, "")
-								.replace(/(><\/)svg>$/, `><title>${this.toolTip || ""}</title></svg>`)
-								.replace(/stroke:#000;/gi, "")
-						: undefined
-				}
-			/>
-		)
+		return <Host innerHTML={this.document} />
 	}
 }
