fecb8c62643c51df2503a224d4c6aad1c8bcfb00
Linus Johansson <linus@issuefab.com>
2024-03-07 12:03:46 +0100

    return types

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -7,31 +7,30 @@ import { Color, Trigger } from "../../model"
 	scoped: true,
 })
 export class SmoothlyDialog {
+	@Element() element: HTMLSmoothlyDialogElement
 	@Prop({ reflect: true }) color: Color | undefined = "primary"
 	@Prop({ mutable: true, reflect: true }) open = true
 	@Prop({ reflect: true }) closable = false
 	@Prop({ reflect: true }) header: string | undefined
-	@Element() element: HTMLSmoothlyDialogElement
+
 	@Listen("click")
-	clickHandler(event: CustomEvent<Trigger>) {
+	clickHandler(event: CustomEvent<Trigger>): void {
 		event.stopPropagation()
 		if (event.composedPath().at(0) == this.element)
 			this.close()
 	}
 	@Method()
-	async close() {
+	async close(): Promise<void> {
 		this.open = false
 	}
-	render() {
+	render(): ReturnType<typeof Host> {
 		return (
 			<Host>
 				<header>
-					{this.closable ? (
+					{this.closable && (
 						<smoothly-button fill="clear" size="flexible" onClick={() => this.close()}>
 							<smoothly-icon name="close-circle" fill="solid" color={this.color}></smoothly-icon>
 						</smoothly-button>
-					) : (
-						[]
 					)}
 					{this.header ? <h1>{this.header}</h1> : <slot name="header"></slot>}
 				</header>
