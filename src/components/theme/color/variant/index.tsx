d7c7e4e22dd8f2c2febd2d72a0fb42fceb03adc3
Linus Johansson <linus@issuefab.com>
2024-03-07 11:56:05 +0100

    remove auxillary work not related to dialog

--- src/components/theme/color/variant/index.tsx
+++ src/components/theme/color/variant/index.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Host, Listen, Prop } from "@stencil/core"
+import { Component, Prop } from "@stencil/core"
 import { Color } from "../../../../model"
 
 @Component({
@@ -7,22 +7,9 @@ import { Color } from "../../../../model"
 	scoped: true,
 })
 export class SmoothlyThemeColorVariant {
-	dialog?: HTMLSmoothlyDialogElement
 	@Prop({ reflect: true }) color: Color
 	@Prop({ reflect: true }) variant: "shade" | "tint" | "color" = "color"
-	@Listen("click")
-	clickHandler() {
-		this.dialog && (this.dialog.open = true)
-	}
 	render() {
-		return (
-			<Host>
-				<smoothly-dialog header="Header" closable open={false} ref={element => (this.dialog = element)}>
-					<h2>test</h2>
-					<p>testar</p>
-				</smoothly-dialog>
-				Aa
-			</Host>
-		)
+		return "Aa"
 	}
 }
