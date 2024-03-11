b45502c04538e7246512d417f20453406e66edc2
Tove Brandt <tove.brandt@pax2pay.com>
2024-03-11 13:46:38 +0100

    label is now positioned correctly

--- src/components/input/range/index.tsx
+++ src/components/input/range/index.tsx
@@ -18,23 +18,24 @@ export class SmoothlyInputRange {
 	valueChanged(): void {
 		this.smoothlyInput.emit({ [this.name]: this.value })
 	}
-	inputHandler(e: Event): void {
+	inputHandler(event: Event): void {
 		this.isClicked = true
-		e.target instanceof HTMLInputElement &&
-			(this.value = this.step !== "any" ? e.target.valueAsNumber : Math.round(e.target.valueAsNumber * 100) / 100)
+		event.target instanceof HTMLInputElement &&
+			(this.value =
+				this.step !== "any" ? event.target.valueAsNumber : Math.round(event.target.valueAsNumber * 100) / 100)
 	}
 
 	render() {
 		return (
-			<Host
-				class={{ "is-clicked": this.isClicked }}
-				style={{ "--left-adjustment": `${(this.value / this.max) * 100}%` }}>
-				<label htmlFor={this.name}>
-					<slot />
-				</label>
-				<output class={`output-left`} htmlFor={this.name}>
-					{this.value}
-				</output>
+			<Host style={{ "--left-adjustment": `${(this.value / this.max) * 100}%` }}>
+				<div>
+					<output class={`output-left`} htmlFor={this.name}>
+						{this.value}
+					</output>
+					<label htmlFor={this.name}>
+						<slot />
+					</label>
+				</div>
 				<input
 					name={this.name}
 					part="range"
@@ -42,7 +43,7 @@ export class SmoothlyInputRange {
 					min={this.min}
 					max={this.max}
 					step={this.step}
-					onInput={e => this.inputHandler(e)}
+					onInput={event => this.inputHandler(event)}
 					value={this.value}
 				/>
 			</Host>
