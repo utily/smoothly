d038bc22accbd047b19b937c29a3485edc0a58c9
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-04-17 11:43:30 +0200

    Added untoggle to filter.

--- src/components/filter/toggle/index.tsx
+++ src/components/filter/toggle/index.tsx
@@ -17,42 +17,45 @@ export class SmoothlyFilterToggle {
 	@Prop({ mutable: true }) active = false
 	@Event() smoothlyFilterUpdate: EventEmitter<Filter.Update>
 	@Event() smoothlyFilterManipulate: EventEmitter<Filter.Manipulate>
+	componentWillLoad() {
+		this.active && this.activeHandler()
+	}
 	componentDidLoad() {
 		this.smoothlyFilterUpdate.emit(this.update.bind(this))
 	}
 	update(expression: selectively.Criteria): void {
-		this.properties.pattern == "Colorpoint" && console.log("update.active", this.active)
-		this.properties.pattern == "Colorpoint" && console.log("expression.expression", expression)
 		if (expression instanceof selectively.And && expression.rules.length > 0) {
 			this.active = Object.entries(this.properties).every(([key, value]) =>
 				expression.rules.some(r => {
 					const criteria = !this.not ? r : r instanceof selectively.Not ? r.criteria : undefined
-					return (
-						criteria?.is({ [key]: value }) &&
-						criteria instanceof selectively.Property &&
-						this.properties[criteria.name] &&
-						criteria.criteria instanceof selectively.Is
-					)
+					return value == ""
+						? !(criteria instanceof selectively.Property && criteria.name == key)
+						: criteria?.is({ [key]: value }) &&
+								criteria instanceof selectively.Property &&
+								this.properties[criteria.name] &&
+								criteria.criteria instanceof selectively.Is
 				})
 			)
 		} else
 			this.active = false
 	}
-	activeHandler() {
+	activeHandler(activate?: true) {
 		const manipulate: Filter.Manipulate = (criteria: selectively.Criteria): selectively.Criteria => {
 			let result: selectively.Criteria = criteria
-			this.active = !this.active
+			activate && (this.active = !this.active)
 			if (result instanceof selectively.And) {
 				for (const [key, value] of Object.entries(this.properties)) {
-					const newCriteria = !this.active
-						? undefined
-						: this.not
-						? selectively.and(selectively.not(selectively.property(key, selectively.is(value))))
-						: selectively.create({ [key]: value })
-					this.properties.pattern == "Colorpoint" && console.log("newCriteria.newCriteria", newCriteria)
+					const newCriteria =
+						!this.active || (this.active && value == "")
+							? undefined
+							: this.not
+							? selectively.not(selectively.property(key, selectively.is(value)))
+							: selectively.create({ [key]: value })
 					const index = result.rules.findIndex(r => this.findInstanceOf(r, key))
-					!newCriteria
+					!newCriteria && index >= 0
 						? result.rules.splice(index, 1)
+						: !newCriteria
+						? undefined
 						: index == -1
 						? result.rules.push(newCriteria)
 						: (result.rules[index] = newCriteria)
@@ -68,14 +71,13 @@ export class SmoothlyFilterToggle {
 		return check instanceof selectively.Property && check.name == property
 	}
 	render() {
-		this.properties.pattern == "Colorpoint" && console.log("this.active", this.active)
 		return (
 			<smoothly-icon
 				fill="clear"
 				color={this.active ? "success" : "medium"}
 				name={(this.active ? `${this.icon}` : `${this.icon}-outline`) as Icon}
 				toolTip={this.toolTip}
-				onClick={() => this.activeHandler()}
+				onClick={() => this.activeHandler(true)}
 			/>
 		)
 	}
