7be8cec825fd13af8a40e32c30211d8965dae16f
tovebr <70133569+tovebr@users.noreply.github.com>
2024-04-18 15:14:36 +0200

    Clicking the reset button in form takes the userout of editmode (#666)

--- src/components.d.ts
+++ src/components.d.ts
@@ -289,7 +289,7 @@ export namespace Components {
     interface SmoothlyInputEdit {
         "color"?: Color;
         "disabled": boolean;
-        "editable": boolean;
+        "display": boolean;
         "expand"?: "block" | "full";
         "fill"?: Fill;
         "shape"?: "rounded";
@@ -1701,7 +1701,7 @@ declare namespace LocalJSX {
     interface SmoothlyInputEdit {
         "color"?: Color;
         "disabled"?: boolean;
-        "editable"?: boolean;
+        "display"?: boolean;
         "expand"?: "block" | "full";
         "fill"?: Fill;
         "onSmoothlyInputLoad"?: (event: SmoothlyInputEditCustomEvent<(parent: HTMLElement) => void>) => void;
