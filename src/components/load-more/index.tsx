07d2cf721c61557da557c3db6390ff6f29d25407
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-04-24 11:05:36 +0200

    Load more can now be used several times. (#679)

--- src/components/load-more/index.tsx
+++ src/components/load-more/index.tsx
@@ -9,20 +9,18 @@ export class LoadMore implements ComponentWillLoad {
 	@Element() element: HTMLSmoothlyLoadMoreElement
 	@Prop() offset = "0"
 	@Prop() name = ""
+	@Prop() multiple = false
 	@Event() smoothlyLoadMore: EventEmitter<string>
 
 	componentWillLoad(): void {
-		const observer: IntersectionObserver = new IntersectionObserver(entries =>
-			this.observationHandler(observer, entries)
-		)
+		const observer = new IntersectionObserver((entries, observer) => this.observationHandler(observer, entries))
 		observer.observe(this.element)
 	}
 
 	observationHandler(observer: IntersectionObserver, entries: IntersectionObserverEntry[]): void {
 		if (entries.find(entry => entry.target == this.element)?.intersectionRatio ?? 0) {
 			this.smoothlyLoadMore.emit(this.name)
-			observer.unobserve(this.element)
-			observer.disconnect()
+			!this.multiple && (observer.unobserve(this.element), observer.disconnect())
 		}
 	}
 
