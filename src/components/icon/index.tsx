fa3d817d7a6f73164850924f57da2c353a0f772c
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-06-06 14:43:20 +0200

    Fix async data race in smoothly icon (#720)
    * Fix possible data race
    
    * Don't return in middle of function
    
    * Make nameChanged own function

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -15,29 +15,41 @@ export class SmoothlyIcon {
 	@Prop({ reflect: true }) rotate?: number
 	@Prop({ reflect: true }) flip?: "x" | "y"
 	@Prop() toolTip?: string
+	@State() latestPromise: Promise<string | undefined>
 	@State() document?: string
-	@Watch("name")
+
 	async componentWillLoad() {
-		let result: string | undefined
+		await this.nameChanged()
+	}
+	@Watch("name")
+	async nameChanged() {
 		if (this.name != "empty") {
-			result = await Icon.load(this.name)
-			result = result
-				?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
-				.replace(` width="512" height="512"`, "")
-				.replace(/stroke:#000;/gi, "")
-			if (!this.toolTip)
-				result = result?.replace(/<title>.*<\/title>/, "")
-			else if (result?.includes("<title>"))
-				result = result.replace(/(<title>).*(<\/title>)/, `<title>${this.toolTip}</title>`)
-			else
-				result = result?.replace(/(.*>)(<\/svg>$)/, `$1<title>${this.toolTip}</title>$2`)
-		}
+			const promise = (this.latestPromise = Icon.load(this.name))
+			let result = await promise
+			if (promise == this.latestPromise) {
+				result = result
+					?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
+					.replace(` width="512" height="512"`, "")
+					.replace(/stroke:#000;/gi, "")
+				if (!this.toolTip)
+					result = result?.replace(/<title>.*<\/title>/, "")
+				else if (result?.includes("<title>"))
+					result = result.replace(/(<title>).*(<\/title>)/, `<title>${this.toolTip}</title>`)
+				else
+					result = result?.replace(/(.*>)(<\/svg>$)/, `$1<title>${this.toolTip}</title>$2`)
+				this.updateDocument(result)
+			}
+		} else
+			this.updateDocument()
+	}
+	updateDocument(document?: string) {
 		this.document =
-			result ??
+			document ??
 			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 		<title>Empty</title>
 		</svg>`
 	}
+
 	render() {
 		return <Host innerHTML={this.document} style={{ ["--rotation"]: `${this.rotate ?? 0}deg` }} />
 	}
