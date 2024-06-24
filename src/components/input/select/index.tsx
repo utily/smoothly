1cd29cc76fef7dddc3bb46c7a15ddeff68e6f0d4
SiFalorSum <nilshansander@hotmail.com>
2024-06-24 16:24:21 +0200

    make select arrows defaults customizable

--- src/components/input/select/index.tsx
+++ src/components/input/select/index.tsx
@@ -39,7 +39,7 @@ export class SmoothlyInputSelect implements Input, Editable, Clearable, Componen
 	@Prop({ reflect: true, mutable: true }) looks: Looks = "plain"
 	@Prop({ reflect: true, mutable: true }) showSelected?: boolean = true
 	@Prop({ reflect: true, mutable: true }) readonly = false
-	@Prop() showArrow = false
+	@Prop() showArrow?: boolean
 	@Prop() multiple = false
 	@Prop() clearable = true
 	@Prop({ mutable: true }) changed = false
@@ -293,7 +293,7 @@ export class SmoothlyInputSelect implements Input, Editable, Clearable, Componen
 				</div>
 				<div class="icons" ref={element => (this.iconsDiv = element)}>
 					<slot name="end" />
-					{(this.looks == "border" || this.showArrow) && (
+					{((this.showArrow == undefined && this.looks == "border") || this.showArrow) && (
 						<smoothly-icon
 							ref={element => (this.toggle = element)}
 							size="tiny"
