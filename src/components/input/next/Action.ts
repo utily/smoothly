ef12f38068591ee41a84e17f96d75866abbb5da5
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-07-27 23:32:15 +0200

    Add deleteWord Backward and FOrward.

--- src/components/input/next/Action.ts
+++ src/components/input/next/Action.ts
@@ -1,5 +1,6 @@
 import { isoly } from "isoly"
 import { tidily } from "tidily"
+import { getAdjacentWordBreakIndex } from "./adjecentWordBreak"
 
 type Formatter = tidily.Formatter & tidily.Converter<any>
 
@@ -54,9 +55,32 @@ export class Action {
 			this.erase(state)
 			return state
 		},
+		deleteWordBackward: (event, _, formattedState) => {
+			event.preventDefault()
+			return this.deleteWord(formattedState, "backward")
+		},
+		deleteWordForward: (event, _, formattedState) => {
+			event.preventDefault()
+			return this.deleteWord(formattedState, "forward")
+		},
 	}
-	public deleteWordBackward(state: tidily.State) {}
-	public deleteWordForward(state: tidily.State) {}
+
+	private deleteWord(formattedState: tidily.State, direction: "backward" | "forward") {
+		const cursorPosition = tidily.Selection.getCursor(formattedState.selection)
+		const adjacentIndex = getAdjacentWordBreakIndex(formattedState.value, cursorPosition, direction)
+		const result = this.unformattedState({
+			...formattedState,
+			selection: {
+				start: Math.min(cursorPosition, adjacentIndex),
+				end: Math.max(cursorPosition, adjacentIndex),
+				direction: "none",
+			},
+		})
+		result.value = result.value.substring(0, result.selection.start) + result.value.substring(result.selection.end)
+		result.selection.end = result.selection.start
+		return result
+	}
+
 	public insertFromPaste(state: tidily.State) {}
 
 	public unformattedState(formattedState: tidily.State) {
