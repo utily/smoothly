46bda81618e41bc1fb08574a9afed1a54a419116
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-07-27 18:58:26 +0200

    Add deleteContentBackward

--- src/components/input/next/Action.ts
+++ src/components/input/next/Action.ts
@@ -4,17 +4,41 @@ type Formatter = tidily.Formatter & tidily.Converter<any>
 
 export namespace Action {
 	export function insertText(formatter: Formatter, state: tidily.State, insertString: string) {
-		if (formatter.allowed(insertString, state)) {
-			replace(state, insertString)
+		const result = unformattedState(formatter, state)
+		if (formatter.allowed(insertString, result)) {
+			replace(result, insertString)
 		}
-		return formatter.format(tidily.StateEditor.copy(state))
+		return formatter.format(tidily.StateEditor.copy(result))
+	}
+	export function deleteContentBackward(formatter: Formatter, state: tidily.State) {
+		const result = unformattedState(formatter, state)
+		if (result.selection.start == result.selection.end)
+			select(result, result.selection.start - 1, result.selection.end)
+		erase(result)
+		return formatState(formatter, state)
 	}
 	export function deleteContentForward(formatter: Formatter, state: tidily.State) {}
-	export function deleteContentBackward(formatter: Formatter, state: tidily.State) {}
-	export function deleteWordForward(formatter: Formatter, state: tidily.State) {}
 	export function deleteWordBackward(formatter: Formatter, state: tidily.State) {}
+	export function deleteWordForward(formatter: Formatter, state: tidily.State) {}
+	export function insertFromPaste(formatter: Formatter, state: tidily.State) {}
 
-	export function replace(state: tidily.State, insertString: string) {
+	export function unformattedState(formatter: Formatter, formattedState: tidily.State) {
+		return tidily.State.copy(formatter.unformat(tidily.StateEditor.copy(formattedState)))
+	}
+	export function formatState(formatter: Formatter, unformattedState: tidily.State) {
+		return formatter.format(tidily.StateEditor.copy(unformattedState))
+	}
+
+	export function select(state: tidily.State, from: number, to: number, direction?: tidily.Direction): void {
+		state.selection.start = from
+		state.selection.end = to
+		direction && (state.selection.direction = direction)
+	}
+
+	function erase(state: tidily.State): void {
+		replace(state, "")
+	}
+	function replace(state: tidily.State, insertString: string) {
 		state.value =
 			state.value.substring(0, state.selection.start) + insertString + state.value.substring(state.selection.end)
 		state.selection.start = state.selection.start + insertString.length
