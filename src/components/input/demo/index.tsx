d19d91301f84d7187ac513f82a52c501894607c6
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-07-31 15:29:56 +0200

    Added composition

--- src/components/input/demo/index.tsx
+++ src/components/input/demo/index.tsx
@@ -14,11 +14,17 @@ export class SmoothlyInputDemo {
 		return (
 			<Host>
 				<smoothly-input-next></smoothly-input-next>
+				<smoothly-input>text</smoothly-input>
 				<smoothly-input-next type="card-number"></smoothly-input-next>
-				<smoothly-input looks="border">text</smoothly-input>
-				<smoothly-input looks="border" type="card-number">
-					card number
-				</smoothly-input>
+				<smoothly-input type="card-number">card-number</smoothly-input>
+				<smoothly-input-next type="email"></smoothly-input-next>
+				<smoothly-input type="email">email</smoothly-input>
+				<smoothly-input-next type="phone"></smoothly-input-next>
+				<smoothly-input type="phone">phone</smoothly-input>
+				<smoothly-input-next type="integer"></smoothly-input-next>
+				<smoothly-input type="integer">integer</smoothly-input>
+				<smoothly-input-next type="password"></smoothly-input-next>
+				<smoothly-input type="password">password</smoothly-input>
 			</Host>
 		)
 	}
