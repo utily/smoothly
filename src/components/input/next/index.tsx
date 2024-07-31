61d513497b0516a3808fe8629d66f0077a1a9beb
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-07-31 16:56:55 +0200

    Set selection in Action class

--- src/components/input/next/index.tsx
+++ src/components/input/next/index.tsx
@@ -15,7 +15,6 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 	@Prop() currency?: isoly.Currency
 	@State() state: Readonly<tidily.State> & Readonly<tidily.Settings>
 	private action: Action
-	private enforceSelection = false // needed to know if selection should be kept
 
 	@Watch("type")
 	typeChange(): void {
@@ -40,21 +39,11 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 			// Get actual selection (can't selection event from all devices)
 			this.state = this.action.onEvent(event, this.state)
 			console.log(event.inputType, (event.target as HTMLInputElement).value, event, event.data, this.state)
-			this.enforceSelection = event.defaultPrevented
 		})
 	}
 	onInput(event: InputEvent) {
 		console.log("onIput", event.inputType, (event.target as HTMLInputElement).value)
 		this.state = this.action.onEvent(event, this.state)
-	}
-	componentDidRender() {
-		if (this.enforceSelection) {
-			this.inputElement.selectionStart = this.state.selection.start
-			this.inputElement.selectionEnd = this.state.selection.end
-			this.inputElement.selectionDirection = this.state.selection.direction ?? null
-			this.enforceSelection = false
-		}
-		// console.log("did render", this.state.selection.start, this.inputElement.selectionStart)
 	}
 
 	render() {
@@ -62,19 +51,13 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 			<Host>
 				<input
 					ref={(e: HTMLInputElement) => (this.inputElement = e)}
-					// value={this.state.value}
 					type={this.state.type}
 					inputMode={this.state.inputmode}
 					placeholder={this.type}
 					onInput={(e: InputEvent) => this.onInput(e)}
 					onCompositionstart={e => (this.state = this.action.onCompositionStart(e, this.state))}
 					onCompositionupdate={e => (this.state = this.action.onCompositionUpdate(e, this.state))}
-					onCompositionend={e => (this.state = this.action.onCompositionEnd(e, this.state))}
-
-					// selectionStart={this.state.selection.start} // TODO see if selection still works on phones
-					// selectionEnd={this.state.selection.end}
-					// selectionDirection={this.state.selection.direction}
-				></input>
+					onCompositionend={e => (this.state = this.action.onCompositionEnd(e, this.state))}></input>
 				<pre style={{ margin: "0" }}>{this.state.value}</pre>
 				<pre style={{ margin: "0" }}>{this.action.composition?.data}</pre>
 			</Host>
