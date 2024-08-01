e78375b03400652cbeb4b6a02f90650df2d0837c
Magnus93 <magnus.ja.gustafsson@gmail.com>
2024-08-01 22:13:07 +0200

    Don show toggle for empty objects

--- src/components/display/json/record/index.tsx
+++ src/components/display/json/record/index.tsx
@@ -11,7 +11,7 @@ export class SmoothlyJsonRecord {
 	@Prop({reflect: true, mutable: true}) open = true
 
 	render(): VNode {
-		return <Host class="object foldable">
+		return <Host class={{empty: Object.keys(this.value).length == 0}}>
 				<span class="open-bracket" onClick={() => this.open = !this.open}>{"{"}</span>
 				<span class="content">
 						{Object.entries(this.value).map(([k, v]) => (
