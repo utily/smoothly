675e939089da3652d1972baa0d8f3628a81e6fd5
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-08-01 11:59:35 +0200

    use AddEventListenerConsistently

--- src/components/input/next/index.tsx
+++ src/components/input/next/index.tsx
@@ -18,6 +18,7 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 
 	@Watch("type")
 	typeChange(): void {
+		// TODO remove previous action listeners if exists. - this.action?.detach(this.inputElement)
 		switch (this.type) {
 			case "price":
 				this.action = Action.create("price", this.currency)
@@ -35,13 +36,14 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 		})
 	}
 	componentDidLoad() {
+		// TODO remove any previous listeners - this.action.attach(this.inputElement)
 		this.inputElement.addEventListener("beforeinput", (event: InputEvent) => {
 			this.state = this.action.onEvent(event, this.state)
 			console.log(event.inputType, (event.target as HTMLInputElement).value, event, event.data, this.state)
 		})
-	}
-	onInput(event: InputEvent) {
-		this.state = this.action.onEvent(event, this.state)
+		this.inputElement.addEventListener("input", (event: InputEvent) => {
+			this.state = this.action.onEvent(event, this.state)
+		})
 	}
 
 	render() {
@@ -51,8 +53,7 @@ export class SmoothlyInputNext implements ComponentWillLoad {
 					ref={(e: HTMLInputElement) => (this.inputElement = e)}
 					type={this.state.type}
 					inputMode={this.state.inputmode}
-					placeholder={this.type}
-					onInput={(e: InputEvent) => this.onInput(e)}></input>
+					placeholder={this.type}></input>
 				<pre style={{ margin: "0" }}>{this.state.value}</pre>
 			</Host>
 		)
