0a7f819364e4483dcee00fac8227624c04e17d29
Tove Brandt <tove.brandt@pax2pay.com>
2024-08-05 09:34:55 +0200

    style

--- src/components/input/demo/index.tsx
+++ src/components/input/demo/index.tsx
@@ -105,7 +105,7 @@ export class SmoothlyInputDemo {
 					Delayed
 				</smoothly-input>
 				<h2>Editable form and Input with Clear and Reset - Readonly</h2>
-				<smoothly-form looks="border" readonly action="https://api.toiletapi.com/6b12fd2f-e896-46f9-b38f-25cf42cee4b4">
+				<smoothly-form looks="grid" readonly action="https://api.toiletapi.com/6b12fd2f-e896-46f9-b38f-25cf42cee4b4">
 					<smoothly-input readonly name="First Name" value="John">
 						First name
 					</smoothly-input>
