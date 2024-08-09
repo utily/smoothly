1228c3f0ad2f3174c4a41f471396d242a61fc0f9
Tove Brandt <tove.brandt@pax2pay.com>
2024-08-09 13:42:16 +0200

    fix

--- src/components/input/range/demo/index.tsx
+++ src/components/input/range/demo/index.tsx
@@ -10,10 +10,10 @@ export class SmoothlyInputRangeDemo {
 				<h2>Range</h2>
 				<h3>Phone Number</h3>
 				<smoothly-input-range min={700000000} max={799999999} step={1} label={"Phone Number"} />
-				<h3>Max 10 step 1 type text</h3>
-				<smoothly-input-range max={10} step={1} label={"Max 10 step 1"} />
+				<h3>Stars out of ten</h3>
+				<smoothly-input-range max={10} step={1} label={"Stars"} />
 				<h3>Percent</h3>
-				<smoothly-input-range type="percent" max={1} step={0.01} label={"Max 100 step 1"} />
+				<smoothly-input-range type="percent" max={1} step={0.01} label={"Percent"} />
 				<h3>With icon at start and clear button</h3>
 				<smoothly-input-range step={1} name="range2" label="Select">
 					<smoothly-icon name="checkmark-circle" slot="start" />
