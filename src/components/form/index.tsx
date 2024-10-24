0f673e7c6e60e9cbf38ba97a1f715beea86d7221
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-10-24 16:27:46 +0200

    formINput emit on load and remove

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -61,7 +61,7 @@ export class SmoothlyForm implements ComponentWillLoad, Clearable, Submittable, 
 	async removeInput(name: string) {
 		this.value = Data.remove(this.value, name)
 		this.inputs.delete(name)
-		console.log("form.removeInput🟪🟪🟪🟪", name, this.value)
+		this.smoothlyFormInput.emit(Data.convertArrays(this.value))
 	}
 	@Method()
 	async listen(property: "changed", listener: (parent: Editable) => Promise<void>): Promise<void> {
@@ -126,7 +126,8 @@ export class SmoothlyForm implements ComponentWillLoad, Clearable, Submittable, 
 				this.contentType = "form-data"
 			this.value = Data.merge(this.value, { [event.target.name]: event.target.value })
 			this.inputs.set(event.target.name, event.target)
-			console.log("form.smoothlyInputLoad🟪🟪🟪🟪", event.target.name, this.value)
+			// console.log("form.smoothlyInputLoad🟪🟪🟪🟪", event.target.name, this.value)
+			this.smoothlyFormInput.emit(Data.convertArrays(this.value))
 		}
 	}
 	@Listen("smoothlyFormDisable")
