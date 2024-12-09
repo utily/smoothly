6947bf498a19a8f453afe060a8aa8aa29eeaf734
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-12-09 10:44:04 +0100

    Rename css variable to --smoothly-button-border-radius (#923)
    * Fix css variable name for button border radius
    
    * remove input-width css var
    
    * remove unused css variable padding

--- src/components/input/demo/index.tsx
+++ src/components/input/demo/index.tsx
@@ -41,8 +41,6 @@ export class SmoothlyInputDemo {
 						showLabel={false}
 						style={{
 							"--smoothly-input-border-radius": "0.5rem",
-							"--padding": "0 0.75em",
-							"--input-width": "12ch",
 						}}>
 						<smoothly-input-clear slot="end" size="icon"></smoothly-input-clear>
 					</smoothly-input-date-range>
