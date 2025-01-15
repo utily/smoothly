f840fa13f7a7469c85626f80fe183ab1564587ed
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-01-15 14:50:15 +0100

    Add modal in demo to show modal with too much contents

--- src/components/dialog/demo/index.tsx
+++ src/components/dialog/demo/index.tsx
@@ -7,6 +7,7 @@ import { Component, h, Host, State } from "@stencil/core"
 })
 export class SmoothlyDialogDemo {
 	@State() openModal = false
+	@State() openTallModal = false
 	@State() showFrame = false
 
 	render() {
@@ -14,6 +15,9 @@ export class SmoothlyDialogDemo {
 			<Host>
 				<smoothly-button fill="solid" color="light" onClick={() => (this.openModal = true)}>
 					Open Modal
+				</smoothly-button>
+				<smoothly-button fill="solid" color="light" onClick={() => (this.openTallModal = true)}>
+					Open Modal with long text
 				</smoothly-button>
 				<smoothly-button fill="solid" color="light" onClick={() => (this.showFrame = !this.showFrame)}>
 					Show Frame
@@ -26,6 +30,24 @@ export class SmoothlyDialogDemo {
 						Confirm
 					</smoothly-button>
 					<smoothly-button slot="actions" color="light" fill="outline" onClick={() => (this.openModal = false)}>
+						Cancel
+					</smoothly-button>
+				</smoothly-modal>
+
+				<smoothly-modal closable open={this.openTallModal} onSmoothlyModalChange={e => (this.openTallModal = e.detail)}>
+					<h2 slot="header">Modal</h2>
+					<div>
+						{Array.from({ length: 10 }).map(() => (
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+								fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+								deserunt mollit anim id est laborum.
+							</p>
+						))}
+					</div>
+					<smoothly-button slot="actions" color="light" fill="outline" onClick={() => (this.openTallModal = false)}>
 						Cancel
 					</smoothly-button>
 				</smoothly-modal>
