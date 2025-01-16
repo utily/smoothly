4c781d3e91d2bc82ce79ed0a6f74ebdb0ba768a7
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-01-16 10:24:17 +0100

    Make scrollbar gutter stable

--- src/components/dialog/demo/index.tsx
+++ src/components/dialog/demo/index.tsx
@@ -36,17 +36,20 @@ export class SmoothlyDialogDemo {
 
 				<smoothly-modal closable open={this.openTallModal} onSmoothlyModalChange={e => (this.openTallModal = e.detail)}>
 					<h2 slot="header">Modal</h2>
-					<div>
-						{Array.from({ length: 10 }).map(() => (
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-								deserunt mollit anim id est laborum.
-							</p>
-						))}
-					</div>
+					<smoothly-summary>
+						<span slot="summary">Summary</span>
+						<div slot="content">
+							{Array.from({ length: 10 }).map(() => (
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+									dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+									aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+									dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+									officia deserunt mollit anim id est laborum.
+								</p>
+							))}
+						</div>
+					</smoothly-summary>
 					<smoothly-button slot="actions" color="light" fill="outline" onClick={() => (this.openTallModal = false)}>
 						Cancel
 					</smoothly-button>
