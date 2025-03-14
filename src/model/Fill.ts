763ea8c663069a014e93e75c0c875716d1ce374a
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-03-14 10:12:42 +0100

    Add Fill to icon demo

--- src/model/Fill.ts
+++ src/model/Fill.ts
@@ -1,1 +1,8 @@
-export type Fill = "clear" | "default" | "outline" | "solid"
+import { isly } from "isly"
+
+export type Fill = typeof Fill.values[number]
+
+export namespace Fill {
+	export const values = ["clear", "default", "outline", "solid"] as const
+	export const type = isly.string(values)
+}
