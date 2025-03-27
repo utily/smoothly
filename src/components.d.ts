4e935aec9bf05c7a4ef1ddf7904349ac30b18534
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-03-27 19:48:55 +0100

    Pad hour and minute in input-date-time

--- src/components.d.ts
+++ src/components.d.ts
@@ -251,6 +251,7 @@ export namespace Components {
         "max"?: number;
         "min"?: number;
         "name": string;
+        "pad"?: number;
         "placeholder": string | undefined;
         "readonly": boolean;
         "register": () => Promise<void>;
@@ -2442,6 +2443,7 @@ declare namespace LocalJSX {
         "onSmoothlyInputLoad"?: (event: SmoothlyInputCustomEvent<(parent: Editable) => void>) => void;
         "onSmoothlyInputLooks"?: (event: SmoothlyInputCustomEvent<(looks?: Looks, color?: Color) => void>) => void;
         "onSmoothlyKeydown"?: (event: SmoothlyInputCustomEvent<Key>) => void;
+        "pad"?: number;
         "placeholder"?: string | undefined;
         "readonly"?: boolean;
         "required"?: boolean;
