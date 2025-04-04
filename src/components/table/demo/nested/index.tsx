9b7397dfadf482803c227b043f521efc099efe86
Marcus Hanses <marcus.hanses@pax2pay.com>
2025-04-04 11:18:35 +0200

    slow

--- src/components/table/demo/nested/index.tsx
+++ src/components/table/demo/nested/index.tsx
@@ -25,19 +25,21 @@ export class SmoothlyTableDemoNested {
 						</smoothly-table-row>
 					</smoothly-table-head>
 					<smoothly-table-body>
-						{data.map(entry => (
-							<smoothly-table-expandable-row>
-								<smoothly-table-demo-nested-inner color="secondary" data={entry.friends} slot={"detail"} />
-								<smoothly-table-cell>{entry.id}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.registered}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.name}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.age}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.balance}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.eyeColor}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.gender}</smoothly-table-cell>
-								<smoothly-table-cell>{entry.company}</smoothly-table-cell>
-							</smoothly-table-expandable-row>
-						))}
+						{Array.from({ length: 20 }).flatMap(() =>
+							data.map(entry => (
+								<smoothly-table-row>
+									{/* <smoothly-table-demo-nested-inner color="secondary" data={entry.friends} slot={"detail"} /> */}
+									<smoothly-table-cell>{entry.id}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.registered}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.name}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.age}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.balance}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.eyeColor}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.gender}</smoothly-table-cell>
+									<smoothly-table-cell>{entry.company}</smoothly-table-cell>
+								</smoothly-table-row>
+							))
+						)}
 					</smoothly-table-body>
 					<smoothly-table-foot>
 						<smoothly-table-row>
