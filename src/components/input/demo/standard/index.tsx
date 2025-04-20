9847bdf4b0138d0282a8d99b020c6a33491ef758
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-04-20 21:53:37 +0200

    disabled prop for date-range

--- src/components/input/demo/standard/index.tsx
+++ src/components/input/demo/standard/index.tsx
@@ -234,7 +234,7 @@ export class SmoothlyInputDemoStandard {
 						color={this.options.color}
 						looks={this.options.looks}
 						readonly={this.options.readonly}
-						// TODO - disabled
+						disabled={this.options.disabled}
 						invalid={this.options.invalid}
 						// TODO - errorMessage
 						placeholder={this.options.placeholder}
