fae917c35403f8192d08cee84e8dd0a22641b985
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-07-07 09:55:52 +0200

    Break out cleanSVg into own function

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -24,28 +24,36 @@ export class SmoothlyIcon {
 	@Watch("toolTip")
 	@Watch("name")
 	async nameChanged() {
-		if (this.name != "empty") {
+		if (this.name == "empty") {
+			this.updateDocument()
+		} else {
 			const promise = (this.latestPromise = Icon.load(this.name))
 			let result = await promise
+
 			if (promise == this.latestPromise) {
-				result = result
-					?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
-					.replace(` width="512" height="512"`, "")
-					.replace(/stroke:#000;/gi, "")
-				if (!this.toolTip)
-					result = result?.replace(/<title>.*<\/title>/, "")
-				else if (result?.includes("<title>"))
-					result = result.replace(/(<title>).*(<\/title>)/, `<title>${this.toolTip}</title>`)
-				else
-					result = result?.replace(/(.*>)(<\/svg>$)/, `$1<title>${this.toolTip}</title>$2`)
-				this.updateDocument(result)
+				if (result) {
+					result = this.cleanSvg(result)
+					this.updateDocument(result)
+				}
 			}
-		} else
-			this.updateDocument()
+		}
 	}
-	updateDocument(document?: string) {
+	private cleanSvg(svg: string): string {
+		svg = svg
+			?.replace(/(?<=^<svg\s?)/, `$& role="img"`)
+			.replace(` width="512" height="512"`, "")
+			.replace(/stroke:#000;/gi, "")
+		if (!this.toolTip)
+			svg = svg?.replace(/<title>.*<\/title>/, "")
+		else if (svg?.includes("<title>"))
+			svg = svg.replace(/(<title>).*(<\/title>)/, `<title>${this.toolTip}</title>`)
+		else
+			svg = svg?.replace(/(.*>)(<\/svg>$)/, `$1<title>${this.toolTip}</title>$2`)
+		return svg
+	}
+	updateDocument(svg?: string) {
 		this.document =
-			document ??
+			svg ??
 			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
 		<title>Empty</title>
 		</svg>`
