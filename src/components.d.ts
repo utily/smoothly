de0ea4343327ba68902fa8a68882222016ea7ecd
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-07-12 03:51:54 +0200

    smoothlyUserInput input-month

--- src/components.d.ts
+++ src/components.d.ts
@@ -1572,6 +1572,7 @@ declare global {
     };
     interface HTMLSmoothlyInputMonthElementEventMap {
         "smoothlyInput": Data;
+        "smoothlyUserInput": Input.UserInput;
         "smoothlyInputLoad": (parent: Editable) => void;
         "smoothlyFormDisable": (disabled: boolean) => void;
         "smoothlyInputLooks": (looks?: Looks, color?: Color) => void;
@@ -2649,6 +2650,7 @@ declare namespace LocalJSX {
         "onSmoothlyInput"?: (event: SmoothlyInputMonthCustomEvent<Data>) => void;
         "onSmoothlyInputLoad"?: (event: SmoothlyInputMonthCustomEvent<(parent: Editable) => void>) => void;
         "onSmoothlyInputLooks"?: (event: SmoothlyInputMonthCustomEvent<(looks?: Looks, color?: Color) => void>) => void;
+        "onSmoothlyUserInput"?: (event: SmoothlyInputMonthCustomEvent<Input.UserInput>) => void;
         "previous"?: boolean;
         "readonly"?: boolean;
         "showLabel"?: boolean;
