e51805225b64c8d2a1f2da254852ed6ed571c0b9
Tove Brandt <tove.brandt@pax2pay.com>
2025-09-24 14:22:36 +0200

    Smoothly modal: Methods to close and open from the outside.

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, h, Host, Prop, Watch } from "@stencil/core"
+import { Component, Event, EventEmitter, h, Host, Method, Prop, Watch } from "@stencil/core"
 
 @Component({
 	tag: "smoothly-modal",
@@ -15,18 +15,26 @@ export class SmoothlyModal {
 	openChanged() {
 		this.smoothlyModalChange.emit(this.open)
 	}
+	@Method()
+	async closeModal() {
+		this.open = false
+	}
+	@Method()
+	async openModal() {
+		this.open = true
+	}
 
 	render() {
 		return (
 			<Host
 				role="alertdialog"
 				onClick={(e: any) => {
-					this.closable && !e.composedPath().some((el: any) => el.classList?.contains("modal")) && (this.open = false)
+					this.closable && !e.composedPath().some((el: any) => el.classList?.contains("modal")) && this.closeModal()
 				}}>
 				<div class="modal">
 					<div class="header">
 						<slot name="header" />
-						{this.closable && <smoothly-icon name="close-outline" fill="solid" onClick={() => (this.open = false)} />}
+						{this.closable && <smoothly-icon name="close-outline" fill="solid" onClick={() => this.closeModal()} />}
 					</div>
 					<slot />
 					<div class="actions">
