d7535e8275c36efa0ae57550055622607083f37a
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-09-26 17:16:19 +0200

    Style separator depending on if it's filled or not

--- src/components/input/date/text/index.tsx
+++ src/components/input/date/text/index.tsx
@@ -168,7 +168,7 @@ export class SmoothlyInputDateRangeText {
 									"smoothly-date-text-part": true,
 									[`smoothly-date-text-${part}`]: true,
 									focused: this.focusedIndex === index,
-									"empty-part": !this.parts[part],
+									"filled-part": (this.parts[part]?.length ?? 0) >= ghost[part].length,
 								}}
 								onFocus={() => (this.focusedIndex = index)}
 								onBlur={() => (this.focusedIndex = undefined)}
