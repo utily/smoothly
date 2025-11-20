5cf7112b2a643ea7c028938ab977dc79d95ff6d7
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2025-11-20 09:57:57 +0100

    Remove extra span

--- src/components/burger/index.tsx
+++ src/components/burger/index.tsx
@@ -33,9 +33,7 @@ export class SmoothlyBurger {
 	render() {
 		return (
 			<Host class={{ "smoothly-burger-visible": this.visible }}>
-				<span class="burger">
-					<smoothly-icon name="menu" />
-				</span>
+				<smoothly-icon name="menu" />
 			</Host>
 		)
 	}
