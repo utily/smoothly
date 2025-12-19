8e85457d3cdac70bd329010d3fb2174b81fbbe6c
tovebr <70133569+tovebr@users.noreply.github.com>
2025-12-19 11:43:40 +0100

    Table head top offset: Prevent overlapping when scrolling trough deeply nested tables. (#1091)
    * Nested tableheads do not overlap.
    
    * different approach
    
    * fixes
    
    * removed unused
    
    * Works
    
    * dropdown and modal z-indexes

--- src/components/table/head/index.tsx
+++ src/components/table/head/index.tsx
@@ -7,8 +7,9 @@ import { Scrollable } from "../../../model"
 	scoped: true,
 })
 export class SmoothlyTableHead {
-	@Element() element: HTMLSmoothlyTableElement
+	@Element() element: HTMLSmoothlyTableHeadElement
 	@State() scrolled?: boolean
+	@State() depth = 0
 	private scrollParent?: HTMLElement
 	private onScroll = (event: Event) => {
 		if (event.target instanceof HTMLElement) {
@@ -18,10 +19,27 @@ export class SmoothlyTableHead {
 			this.scrolled = parentRect.top == elementRect.top
 		}
 	}
-
+	getTopOffset() {
+		let depth = 0
+		let currentElement: HTMLElement | null = this.element
+		while (currentElement) {
+			if (currentElement.tagName.toLowerCase() === "smoothly-table") {
+				const head = currentElement.querySelector("smoothly-table-head") as HTMLSmoothlyTableHeadElement
+				if (head !== this.element) {
+					const rows = Array.from(
+						head.querySelectorAll("smoothly-table-row") as NodeListOf<HTMLSmoothlyTableHeadElement>
+					)
+					depth += rows.length
+				}
+			}
+			currentElement = currentElement.parentElement as HTMLSmoothlyTableHeadElement
+		}
+		this.depth = depth
+	}
 	connectedCallback() {
 		this.scrollParent = Scrollable.findParent(this.element)
 		this.scrollParent?.addEventListener("scroll", this.onScroll)
+		this.getTopOffset()
 	}
 	disconnectedCallback() {
 		this.scrollParent?.removeEventListener("scroll", this.onScroll)
@@ -29,7 +47,14 @@ export class SmoothlyTableHead {
 
 	render(): VNode | VNode[] {
 		return (
-			<Host class={{ scrolled: !!this.scrolled }}>
+			<Host
+				class={{ scrolled: !!this.scrolled }}
+				style={
+					{
+						"--top": `calc(${this.depth} * var(--smoothly-table-cell-min-height))`,
+						"--z-index": `${10 - this.depth}`,
+					} as any
+				}>
 				<slot />
 			</Host>
 		)
