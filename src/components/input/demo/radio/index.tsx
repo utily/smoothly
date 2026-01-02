b0eb937f76b94e3893dd17402e161ec0541844b1
qijieye <qijie.ye@pax2pay.com>
2026-01-02 15:53:39 +0100

    using css to show/hide slot content

--- src/components/input/demo/radio/index.tsx
+++ src/components/input/demo/radio/index.tsx
@@ -22,32 +22,29 @@ export class SmoothlyInputDemoRadio {
 					onSmoothlyInput={e => (this.selected = e.detail.radio as string)}>
 					<smoothly-input-radio-item value="1" selected>
 						<span>Option 1</span>
-						{this.selected == "1" && (
-							<span slot="detail">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-								ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-								fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-								deserunt mollit anim id est laborum.
-							</span>
-						)}
+
+						<span slot="detail">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+							ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+							fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+							mollit anim id est laborum.
+						</span>
 					</smoothly-input-radio-item>
 					<smoothly-input-radio-item value="2">
 						<span>Option 2</span>
 						<div slot="detail">
-							{this.selected == "2" && (
-								<smoothly-tabs>
-									<smoothly-tab label="Dog" name="dog" open>
-										🐕
-									</smoothly-tab>
-									<smoothly-tab label="Cat" name="cat">
-										🐈
-									</smoothly-tab>
-									<smoothly-tab label="Rat" name="rat">
-										🐀
-									</smoothly-tab>
-								</smoothly-tabs>
-							)}
+							<smoothly-tabs>
+								<smoothly-tab label="Dog" name="dog" open>
+									🐕
+								</smoothly-tab>
+								<smoothly-tab label="Cat" name="cat">
+									🐈
+								</smoothly-tab>
+								<smoothly-tab label="Rat" name="rat">
+									🐀
+								</smoothly-tab>
+							</smoothly-tabs>
 						</div>
 					</smoothly-input-radio-item>
 				</smoothly-input-radio>
