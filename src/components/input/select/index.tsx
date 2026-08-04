a45eace63e247a5298f7f14b561a728d264a3a25
Tove Brandt <tove.brandt@pax2pay.com>
2026-08-04 09:28:02 +0200

    separator

--- src/components/input/select/index.tsx
+++ src/components/input/select/index.tsx
@@ -40,6 +40,7 @@ export class SmoothlyInputSelect implements Input, Editable, Clearable, Componen
 	private searchElement?: HTMLInputElement
 	private items: HTMLSmoothlyItemElement[] = []
 	private itemHeight: number | undefined
+	private readonly displaySeparator = '<span style="display:inline-block;width:0.8rem"></span>'
 	@Element() element: HTMLSmoothlyInputSelectElement
 	@Prop({ reflect: true }) invalid?: boolean = false
 	@Prop({ reflect: true }) errorMessage?: string
@@ -352,7 +353,11 @@ export class SmoothlyInputSelect implements Input, Editable, Clearable, Componen
 				class={{ "has-value": this.selected.length !== 0, open: this.open, "has-filter": this.filter !== "" }}
 				onClick={(e: MouseEvent) => this.onClick(e)}>
 				<div class="select-display">
-					{this.display.length > 0 ? <div class="value" innerHTML={this.display.join("  ")}></div> : this.placeholder}
+					{this.display.length > 0 ? (
+						<div class="value" innerHTML={this.display.join(this.displaySeparator)}></div>
+					) : (
+						this.placeholder
+					)}
 				</div>
 				<div class="icons" ref={element => (this.iconsElement = element)}>
 					<smoothly-icon class="smoothly-invalid" name="alert-circle" size="small" tooltip={this.errorMessage} />
